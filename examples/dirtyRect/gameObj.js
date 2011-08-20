c1b9a98df429f287972760029fec8f9226a72bb2
obiot <olivier.biot@gmx.com>
2011-08-20 16:02:09 +0800

    Added an example (based on the Tutorial) and using the DirtyRect feature. Although quite basic for now, it will be completed along the dirtyRect feature in the engine.
    If there is any beta tester out there, I would really be interested by
    some feedback on this, thank you :)

---
+++ examples/dirtyRect/gameObj.js
@@ -0,0 +1,138 @@
+/* -----
+
+game object
+   
+------   */
+
+/*------
+   
+   a player entity
+   
+ -------*/
+var PlayerEntity = me.ObjectEntity.extend(
+{	
+   
+   /* -----
+
+   constructor
+   
+   ------ */
+   
+   init:function (x, y, settings)
+   {
+      // call the constructor
+      this.parent(x, y , settings);
+      
+      // set the walking & jumping speed
+      this.setVelocity(3, 15);
+      
+      // adjust the bounding box
+      this.updateColRect(8,48, -1,0);
+      
+      // set the display to follow our position on both axis
+      me.game.viewport.follow(this.pos, me.game.viewport.AXIS.HORIZONTAL);
+   },
+
+   /* -----
+   
+      update the player pos
+      
+      ------ */
+   update : function ()
+   {
+      if (me.input.isKeyPressed('left'))
+      {
+         this.doWalk(true);
+      }
+      else if (me.input.isKeyPressed('right'))
+      {
+         this.doWalk(false);
+      }
+      else
+      {
+         this.vel.x = 0;
+      }
+      if (me.input.isKeyPressed('jump'))
+      {	
+         this.doJump();
+      }
+      
+      // check & update player movement
+      updated = this.updateMovement();
+      
+      // check for collision
+      res = me.game.collide(this);
+      
+      if (res)
+      {
+         if (res.type == me.game.ENEMY_OBJECT)
+         {
+            if ((res.y>0) && !this.jumping)
+            {  
+               // bounce
+               this.forceJump();
+            }
+            else
+            {
+               // let's flicker in case we touched an enemy
+               this.flicker(45);
+            }
+         }
+      }
+            
+      // update animation
+      if (updated)
+      {
+         // update objet animation
+         this.parent(this);
+      }
+      return updated;
+   }
+
+});
+
+/*------
+   
+   a coin entity
+   
+ -------*/
+var CoinEntity = me.CollectableEntity.extend(
+{	
+   init: function (x, y, settings)
+   {
+      // call the parent constructor
+      this.parent(x, y , settings);
+   },		
+      
+   onDestroyEvent : function ()
+   {
+      // give some score
+      me.game.HUD.updateItemValue("score", 250);
+   }
+   
+});
+
+/*------
+   
+   a score HUD item
+   
+ -------*/
+var ScoreObject = me.HUD_Item.extend(
+{	
+   init: function(x, y)
+   {
+      // call the parent constructor
+      this.parent(x, y);
+      // create a font
+      this.font = new me.BitmapFont("32x32_font", 32);
+   },
+   
+   //	draw our score
+   draw : function (context, x, y)
+   {
+      this.font.draw (context, this.value, this.pos.x +x, this.pos.y+y);
+   }
+
+});
+
+
---
+++ examples/dirtyRect/main.js
@@ -0,0 +1,145 @@
+/*!
+ * 
+ *   melonJS
+ *   http://www.melonjs.org
+ *		
+ *   Step by step game creation tutorial
+ *
+ **/
+
+// game resources
+var g_resources= [  
+                     // our level tileset
+                     {name: "area01_level_tiles",  type:"image",	src: "data/area01_tileset/area01_level_tiles.png"},
+                     // our levels
+                     {name: "area01",              type: "tmx",	src: "data/area01.tmx"},
+                     // the main player spritesheet
+                     {name: "gripe_run_right",     type:"image",	src: "data/sprite/gripe_run_right.png"},
+                     // the parallax background
+                     {name: "area01_bkg0",         type:"image",	src: "data/area01_parallax/area01_bkg0.png"},
+                     {name: "area01_bkg1",         type:"image",	src: "data/area01_parallax/area01_bkg1.png"},
+                     // the spinning coin spritesheet
+                     {name: "spinning_coin_gold",  type:"image",	src: "data/sprite/spinning_coin_gold.png"},
+                     // our enemty entity
+                     {name: "wheelie_right",       type:"image",	src: "data/sprite/wheelie_right.png"},
+                     // game font
+                     {name: "32x32_font",          type:"image",	src: "data/sprite/32x32_font.png"},
+                      // title screen
+                     {name: "title_screen",        type:"image",	src: "data/GUI/title_screen.png"},
+                  ]; 
+
+
+var jsApp	= 
+{	
+   /* ---
+	
+      Initialize the jsApp
+         
+      ---			*/
+   onload: function()
+   {
+		
+      // enable the dirtyRect feature
+      me.sys.dirtyRegion = true;
+      
+      // some debug (draw the dirty area, if true)
+      me.debug.renderDirty = false;
+      
+      
+      // init the video
+      if (!me.video.init('jsapp', 1024, 768))
+      {
+         alert("Sorry but your browser does not support html 5 canvas.");
+         return;
+      }
+		
+				
+      // initialize the "audio"
+      me.audio.init("mp3,ogg");
+		
+      // set all resources to be loaded
+      me.loader.onload = this.loaded.bind(this);
+		
+      // set all resources to be loaded
+      me.loader.preload(g_resources);
+
+      // load everything & display a loading screen
+      me.state.change(me.state.LOADING);
+   },
+	
+	
+   /* ---
+
+		callback when everything is loaded
+   ---										*/
+   loaded: function ()
+   {
+      // set the "Play/Ingame" Screen Object
+      me.state.set(me.state.MENU, new TitleScreen());
+      
+      // set the "Play/Ingame" Screen Object
+      me.state.set(me.state.PLAY, new PlayScreen());
+      
+      // NOT WORKING WITH Dirty Rect..
+      // Screen is not refresh when the fading is over
+      // set a global fading transition for the screen
+		//me.state.transition("fade", "#FFFFFF", 15);
+      
+      // add our Player entity & Coin in the entity pool
+      me.entityPool.add("mainPlayer", PlayerEntity);
+      me.entityPool.add("CoinEntity", CoinEntity);
+      
+			
+      // enable the keyboard
+      me.input.bindKey(me.input.KEY.LEFT,    "left");
+      me.input.bindKey(me.input.KEY.RIGHT,   "right");
+      me.input.bindKey(me.input.KEY.X,       "jump", true);
+      
+      // BUG WITH Dirty rec
+      // Menu screen is broken when using dirty rect
+      // SO start the game directly !
+      me.state.change(me.state.PLAY);
+	}
+
+}; // jsApp
+
+/* the in game stuff*/
+var PlayScreen = me.ScreenObject.extend(
+{
+
+   onResetEvent: function()
+   {	
+      // load a level
+      me.levelDirector.loadLevel("area01");
+      
+      // add a default HUD to the game mngr
+      me.game.addHUD(850,0,174,50);
+		
+      // add a new HUD item 
+      me.game.HUD.addItem("score", new ScoreObject(170,10));
+		
+      // make sure everyhting is in the right order
+      me.game.sort();
+
+   },
+	
+	
+   /* ---
+
+      action to perform when game is finished (state change)
+
+   ---	*/
+   onDestroyEvent: function()
+   {  
+      // remove the HUD
+      me.game.disableHUD();
+   }
+
+});
+
+
+//bootstrap :)
+window.onReady(function() 
+{
+	jsApp.onload();
+});
