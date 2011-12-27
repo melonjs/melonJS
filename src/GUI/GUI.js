a9910cb9455b3abc5dede8021bc96bb4bccbcd31
obiot <olivier.biot@gmx.com>
2011-12-27 19:19:39 +0800

    Corrected the GUI Object, added proper documentation (was missing), and marked it as deprecated as it should be rewritten in a next release.

--- src/GUI/GUI.js
+++ src/GUI/GUI.js
@@ -6,39 +6,74 @@
  */
 
 (function($, undefined) {
-
-	/************************************************************************************/
-	/*      GUI FUNCTIONS :                                                             */
-	/*      a basic set of objects to manage GUI elements                               */
-	/*      basically they are sprite object with some event management                 */
-	/************************************************************************************/
-
-	/* -----
-
-		a Simple GUI object
-			
-		------									*/
-	var GUI_Object = me.SpriteObject.extend({
-		// object can be clicked or not
+	
+	/**
+	 * GUI Object<br>
+	 * A basic object to manage GUI elements <br>
+	 * @deprecated won't survive version 0.9.3
+	 * @class
+	 * @extends me.SpriteObject
+	 * @memberOf me
+	 * @example
+	 * // Enable Mouse Event Management
+	 * // should be ideally in the main at initialization
+	 * me.input.enableMouseEvent(true);
+	 *
+	 * // create a basic GUI Object
+	 * var myButton = me.GUI_Object.extend(
+	 * {	
+	 *    init:function(x, y)
+	 *   {
+	 *      settings = {}
+	 *      settings.image = "button";
+	 *      settings.spritewidth = 100;
+	 *      settings.spriteheight = 50;
+	 *      // parent constructor
+	 *      this.parent(x, y, settings);
+	 *   },
+	 *	
+	 *   // output something in the console
+	 *   // when the object is clicked
+	 *   clicked:function()
+	 *   {
+	 *      console.log("clicked!");
+	 *      return false;
+	 *   }
+	 * });
+	 * 
+	 * // add the object at pos (10,10), z index 4
+	 * me.game.add((new myButton(10,10)),4);
+	 *
+	 */
+	GUI_Object = me.SpriteObject.extend({
+	/** @scope me.GUI_Object.prototype */
+	
+		/**
+		 * object can be clicked or not
+		 * @public
+		 * @type boolean
+		 * @name me.GUI_Object#isClickable
+		 */
 		isClickable : true,
+		
 		// object has been updated (clicked,etc..)	
 		updated : false,
 
-		/* -----
-
-			constructor function
-				
-			------ */
-
-		init : function(x, y, image, spritewidth) {
-			this.parent(this, x, y, image, spritewidth);
+		/**
+		 * @Constructor
+		 * @private
+		 */
+		 init : function(x, y, settings) {
+			this.parent(x, y, 
+						((typeof settings.image == "string") ? me.loader.getImage(settings.image) : settings.image), 
+						settings.spritewidth, 
+						settings.spriteheight);
 		},
 
-		/* -----
-
-			update function
-				
-			------ */
+		/**
+		 * return true if the object has been clicked
+		 * @private
+		 */
 		update : function() {
 			if (this.updated) {
 				// clear the flag
@@ -48,25 +83,28 @@
 			return false;
 		},
 
-		/* -----
-
-			a clicked function called when the object is clicked
-			return true if the object is to be redraw
-				
-			------									*/
+		
+		/**
+		 * function called when the object is clicked <br>
+		 * to be overwritten <br>
+		 * return true if object need to be updated
+		 * @public
+		 * @function
+		 * @deprecated won't survive version 0.9.3
+		 */
 		clicked : function() {
 			return false;
 		},
 
-		/* -----
 
-			mouse event detection
-				
-			------									*/
+		/**
+		 * mouse event detection
+		 * @private
+		 */
 		mouseEvent : function(x, y) {
-			if ((x > this.pos.x) && (x < this.pos.x + this.displayWidth)
+			if ((x > this.pos.x) && (x < this.pos.x + this.width)
 					&& (y > this.pos.y)
-					&& (y < this.pos.y + this.displayHeight)) {
+					&& (y < this.pos.y + this.height)) {
 				// notify the object we have been clicked :)
 				if (this.isClickable) {
 					this.updated = this.clicked();
