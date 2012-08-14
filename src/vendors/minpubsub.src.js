d04a5246faf1b0e45530d064cd98a6904b8afd41
obiot <olivier.biot@gmx.com>
2012-08-14 19:31:51 +0800

    Updated with jsDoc style documentation
    (not working, but i'll finish later, i'm hungry!)

--- src/vendors/minpubsub.src.js
+++ src/vendors/minpubsub.src.js
@@ -9,21 +9,21 @@
 	// the topic/subscription hash
 	var cache = d.c_ || {}; //check for "c_" cache for unit testing
 	
-	d.publish = function(/* String */ topic, /* Array? */ args){
-		// summary: 
-		//		Publish some data on a named topic.
-		// topic: String
-		//		The channel to publish on
-		// args: Array?
-		//		The data to publish. Each array item is converted into an ordered
-		//		arguments on the subscribed functions. 
-		//
-		// example:
-		//		Publish stuff on '/some/topic'. Anything subscribed will be called
-		//		with a function signature like: function(a,b,c){ ... }
-		//
-		//		publish("/some/topic", ["a","b","c"]);
-		
+	/*
+	 * Publish some data on a channel
+	 * @public
+	 * @function
+	 * @param {String} topic The topic to publish on
+	 * @param {Array} arguments The data to publish
+	 *
+	 * @example Publish stuff on '/some/topic'.
+	 * Anything subscribed will be called with a function
+	 * signature like: function(a,b,c){ ... }
+	 *
+	 * me.publish("/some/topic", ["a","b","c"]);
+	 * 
+	 */
+	me.publish = function(topic, args){
 		var subs = cache[topic],
 			len = subs ? subs.length : 0;
 
@@ -33,38 +33,40 @@
 		}
 	};
 
-	d.subscribe = function(/* String */ topic, /* Function */ callback){
-		// summary:
-		//		Register a callback on a named topic.
-		// topic: String
-		//		The channel to subscribe to
-		// callback: Function
-		//		The handler event. Anytime something is publish'ed on a 
-		//		subscribed channel, the callback will be called with the
-		//		published array as ordered arguments.
-		//
-		// returns: Array
-		//		A handle which can be used to unsubscribe this particular subscription.
-		//	
-		// example:
-		//		subscribe("/some/topic", function(a, b, c){ /* handle data */ });
+	/*
+	 * Register a callback on a named topic.
+	 * @public
+	 * @function
+	 * @param {String} channel The channel to subscribe to
+	 * @param {Function} callback The event handler, any time something is
+	 * published on a subscribed channel, the callback will be called
+	 * with the published array as ordered arguments
+	 * @return Array A handle which can be used to unsubscribe this
+	 * particular subscription
+	 * @example
+	 * me.subscribe("/some/topic", function(a, b, c){ doSomething(); });
+	 */
 
+	me.subscribe = function(topic, callback){
 		if(!cache[topic]){
 			cache[topic] = [];
 		}
 		cache[topic].push(callback);
 		return [topic, callback]; // Array
 	};
-
-	d.unsubscribe = function(/* Array */ handle, /* Function? */ callback){
-		// summary:
-		//		Disconnect a subscribed function for a topic.
-		// handle: Array
-		//		The return value from a subscribe call.
-		// example:
-		//		var handle = subscribe("/some/topic", function(){});
-		//		unsubscribe(handle);
-		
+	
+	/*
+	 * Disconnect a subscribed function for a topic.
+	 * @public
+	 * @function
+	 * @param {Array} handle The return value from a subscribe call or the
+	 * name of a channel as a String
+	 * @param {Function} [callback] The return value from a subscribe call.
+	 * @example
+	 * var handle = me.subscribe("/some/channel", function(){});
+	 * me.unsubscribe(handle);
+	 */
+	me.unsubscribe = function(handle, callback){
 		var subs = cache[callback ? handle : handle[0]],
 			callback = callback || handle[1],
 			len = subs ? subs.length : 0;
@@ -76,4 +78,4 @@
 		}
 	};
 
-})(me);
+})(this);
