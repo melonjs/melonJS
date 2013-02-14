40248f1178ee87072450c5881bd3c4cf31db33c4
obiot <olivier.biot@gmx.com>
2013-02-14 16:35:34 +0800

    rewrote these two functions with better self-caching

--- src/level/TMXTileset.js
+++ src/level/TMXTileset.js
@@ -143,11 +143,7 @@
 			// tile properties
 			// (collidable, etc..)
 			this.TileProperties = [];
-			
-			// a cache for offset value
-			this.tileXOffset = [];
-			this.tileYOffset = [];
-
+						
 			if (!this.image) {
 				console.log("melonJS: '" + imagesrc + "' file for tileset '" + this.name + "' not found!");
 			} else {
@@ -260,25 +256,33 @@
 		/**
 		 * return the x offset of the specified tile in the tileset image
 		 * @private
-		 */
-		getTileOffsetX : function(tileId) {
-			if (this.tileXOffset[tileId] == null) {
-				this.tileXOffset[tileId] = this.margin + (this.spacing + this.tilewidth)  * (tileId % this.hTileCount);
-			}
-			return this.tileXOffset[tileId];
-		},
+		 */	
+		getTileOffsetX : (function() {
+			var cache = [];
+			return function (tileId) {
+				var cached = cache[tileId];
+				if (cached === undefined) {
+					return (cache[tileId] = this.margin + (this.spacing + this.tilewidth)  * (tileId % this.hTileCount));
+				}
+				return cached;
+			};
+		}()),
+		
 		
 		/**
 		 * return the y offset of the specified tile in the tileset image
 		 * @private
 		 */
-		getTileOffsetY : function(tileId) {
-			if (this.tileYOffset[tileId] == null) {
-				this.tileYOffset[tileId] = this.margin + (this.spacing + this.tileheight)	* ~~(tileId / this.hTileCount);
-			}
-			return this.tileYOffset[tileId];
-		},
-
+		getTileOffsetY : (function() {
+			var cache = [];
+			return function (tileId) {
+				var cached = cache[tileId];
+				if (cached === undefined) {
+					return (cache[tileId] = this.margin + (this.spacing + this.tileheight)	* ~~(tileId / this.hTileCount));
+				}
+				return cached;
+			};
+		}()),
 
 		// draw the x,y tile
 		drawTile : function(context, dx, dy, tmxTile) {
