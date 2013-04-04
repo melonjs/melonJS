20ac46f5eed17386d66beed20a976c811f361bf9
Jason Oster <jay@kodewerx.org>
2013-04-04 16:13:34 -0700

    Do not call `obj.destroy()` early in `me.game.remove()` (the removal may be deferred)
    - Fixes some questionable uses of `me.game.remove()` inside callbacks.
    - Calling `me.game.remove()` from a callback with `force == true` or `me.game.removeAll()` will NEVER be safe. Update docs to include this warning.

--- src/core.js
+++ src/core.js
@@ -1443,38 +1443,45 @@ var me = me || {};
 		 * @public
 		 * @function
 		 * @param {me.ObjectEntity} obj Object to be removed
-		 * @param {Boolean} force force immediate deletion
+		 * @param {Boolean=false} force force immediate deletion.<br>
+		 * <strong>WARNING</strong>: Not safe to use asynchronously (e.g. onCollision callbacks)
 		 */
 		api.remove = function(obj, force) {
-			
-			// notify the object it will be destroyed
-			if (obj.destroy) {
-				obj.destroy();
+
+			// Private function to do object removal
+			function removeNow(target) {
+				// notify the object it will be destroyed
+				if (target.destroy) {
+					target.destroy();
+				}
+
+				// remove the object from the object to draw
+				drawManager.remove(target);
+
+				// Remove the object
+				gameObjects.remove(target);
+				me.entityPool.freeInstance(target);
 			}
-			
-			// remove the object from the object to draw
-			drawManager.remove(obj);
-			
+
 			// remove the object from the object list
 			if (force===true) {
 				// force immediate object deletion
-				gameObjects.remove(obj);
-				me.entityPool.freeInstance(obj);
+				removeNow(obj);
 			} else {
 				// make it invisible (this is bad...)
 				obj.visible = false;
 				// else wait the end of the current loop
 				/** @private */
 				pendingRemove = (function (obj) {
-					gameObjects.remove(obj);
-					me.entityPool.freeInstance(obj);
+					removeNow(obj);
 					pendingRemove = null;
 				}).defer(obj);
 			}
 		};
 
 		/**
-		 * remove all objects
+		 * remove all objects<br>
+		 * <strong>WARNING</strong>: Not safe to use asynchronously (e.g. onCollision callbacks)
 		 * @name me.game#removeAll
 		 * @public
 		 * @function
