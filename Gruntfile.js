10dd31bad3ca91ce5094f521aa1cbbad6c2f25c6
obiot <olivier.biot@gmx.com>
2013-04-14 19:43:01 +0800

    [#167] upgraded to grunt 0.4 and fixed the build system
    Currently broken (todo) :
    * jshint : generates too much error and is not part for now of the
    default build option (source code need lots of fixes first)
    * coffeescript : requires a new .coffeescript file (can anyone help?)
    * banner: removed from now as format changed with grunt 0.4
    * jsdoc; to be added (together with a new template for version 3.x)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,81 +3,83 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
-    lint: {
-      files: ['src/**/*.js']
-    },
+    pkg: grunt.file.readJSON('package.json'),
+
     concat: {
-      dist: {
-        src: [
-          '<banner:meta.banner>',
-          	'src/core.js',
-			'src/math/geometry.js',
-			'src/debug/debug.js',
-			'src/renderable/base.js',
-			'src/renderable/sprite.js',
-			'src/renderable/texturepacker.js',
-			'src/renderable/camera.js',
-			'src/entity/entity.js',
-			'src/state/state.js',
-			'src/loader/loader.js',
-			'src/font/font.js',
-			'src/GUI/GUI.js',
-			'src/GUI/HUD.js',
-			'src/audio/audio.js',
-			'src/video/video.js',
-			'src/input/input.js',
-			'src/utils/utils.js',
-			'src/utils/stat.js',
-			'src/level/TMXConstants.js',
-			'src/level/TMXUtils.js',
-			'src/level/TMXObjectGroup.js',	
-			'src/level/TMXTileset.js',
-			'src/level/TMXRenderer.js',
-			'src/level/TMXLayer.js',
-			'src/level/TMXTiledMap.js',
-			'src/level/TMXMapReader.js',
-			'src/level/LevelDirector.js',
-			'src/vendors/tween.js',
-			'src/vendors/minpubsub.src.js',
-			'src/plugin/plugin.js'
-        ],
-        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
-      }
+		dist: {
+			src: [
+				'src/core.js',
+				'src/math/geometry.js',
+				'src/debug/debug.js',
+				'src/renderable/base.js',
+				'src/renderable/sprite.js',
+				'src/renderable/texturepacker.js',
+				'src/renderable/camera.js',
+				'src/entity/entity.js',
+				'src/state/state.js',
+				'src/loader/loader.js',
+				'src/font/font.js',
+				'src/GUI/GUI.js',
+				'src/GUI/HUD.js',
+				'src/audio/audio.js',
+				'src/video/video.js',
+				'src/input/input.js',
+				'src/utils/utils.js',
+				'src/utils/stat.js',
+				'src/level/TMXConstants.js',
+				'src/level/TMXUtils.js',
+				'src/level/TMXObjectGroup.js',	
+				'src/level/TMXTileset.js',
+				'src/level/TMXRenderer.js',
+				'src/level/TMXLayer.js',
+				'src/level/TMXTiledMap.js',
+				'src/level/TMXMapReader.js',
+				'src/level/LevelDirector.js',
+				'src/vendors/tween.js',
+				'src/vendors/minpubsub.src.js',
+				'src/plugin/plugin.js'
+			],
+			dest: 'build/<%= pkg.name %>-<%= pkg.version %>.js'
+		}
     },
-    min: {
-      dist: {
-        src: ['<banner:meta.banner>', '<config:concat.dist.dest>'],
-        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js'
-      }
-    },
+	
+	uglify: {
+		options: {
+			banner: "/*! melonJS (c)2011 - 2013 Olivier Biot (http://www.melonjs.org) */\n",
+		},
+		dist: {
+			files: {
+				'build/<%= pkg.name %>-<%= pkg.version %>.min.js': ['<%= concat.dist.dest %>']
+			}
+		}
+	},
+    
+	
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {}
-    },
-    uglify: {}
+		dist: {
+			src: [ 'build/<%= pkg.name %>-<%= pkg.version %>.js' ]
+		},
+		options: {
+			curly: true,
+			eqeqeq: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			sub: true,
+			undef: true,
+			boss: true,
+			eqnull: true,
+			browser: true
+		}
+   }
   });
 
-  // Default task.
-  grunt.registerTask('default', 'lint concat min');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+ 
+ // Default task.
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
