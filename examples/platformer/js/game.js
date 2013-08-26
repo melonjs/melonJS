d8735e5ca23d1f47b3c3e01dabdae7a262242148
Olivier Biot <olivier.biot@me.com>
2013-08-26 20:43:18 +0800

    Fixed debugPanel installation when `#debug` is defined

--- examples/platformer/js/game.js
+++ examples/platformer/js/game.js
@@ -22,9 +22,13 @@ var game = {
 			alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
 			return;
 		}
-		
-		// install the debug panel plugin
-		//me.plugin.register(debugPanel, "debug");
+
+		// add "#debug" to the URL to enable the debug Panel
+		if (document.location.hash === "#debug") {
+			window.onReady(function () {
+				me.plugin.register.defer(debugPanel, "debug");
+			});
+		}
 		
 		// initialize the "sound engine"
 		me.audio.init("mp3,ogg");
