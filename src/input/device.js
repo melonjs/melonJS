ba609c264c036bd9af3f067439ad97218de7204d
Aaron McLeod <sircoolguy99@gmail.com>
2013-09-15 18:14:18 -0400

    created device script, moved device motion logic from input to the device namespace.

---
+++ src/input/device.js
@@ -0,0 +1,120 @@
+(function(window) {
+	me.device = (function() {
+		// defines object for holding public information/functionality.
+		var obj = {};
+
+		/**
+		 * Accelerometer information<br>
+		 * properties : x, y, z
+		 * @public
+		 * @enum {number}
+		 * @name accel
+		 * @memberOf me.device
+		 */
+		obj.accel = {
+			x: 0, 
+			y: 0, 
+			z: 0
+		};
+
+		/** 
+		 * Device Orientation. Stores angle in degrees for each axis.
+		 * properties : tiltLeftRight, tiltFrontBack, direction
+		 * @public
+		 * @name orientation
+		 * @memberOf me.device
+		 */
+		obj.orientation = {
+			tiltLeftRight: 0,
+			tiltFrontBack: 0,
+			direction: 0
+		};
+
+		/**
+		 * watch Accelerator event 
+		 * @name watchAccelerometer
+		 * @memberOf me.device
+		 * @public
+		 * @function
+		 * @return {boolean} false if not supported by the device
+		 */
+		obj.watchAccelerometer = function () {
+			if (me.sys.hasAccelerometer) {
+				if (!accelInitialized) {
+					if (typeof Windows === 'undefined') {
+						// add a listener for the devicemotion event
+						window.addEventListener('devicemotion', onDeviceMotion, false);
+					} else {
+						// On Windows 8 Device
+						var accelerometer = Windows.Devices.Sensors.Accelerometer.getDefault();
+						if (accelerometer) {
+							// Capture event at regular intervals
+							var minInterval = accelerometer.minimumReportInterval;
+							var Interval = minInterval >= 16 ? minInterval : 25;
+							accelerometer.reportInterval = Interval;
+
+							accelerometer.addEventListener('readingchanged', onDeviceMotion, false);
+						}
+					}
+					accelInitialized = true;
+				}
+				return true;
+			}
+			return false;
+		};
+		
+		/**
+		 * unwatch Accelerometor event 
+		 * @name unwatchAccelerometer
+		 * @memberOf me.device
+		 * @public
+		 * @function
+		 */
+		obj.unwatchAccelerometer = function() {
+			if (accelInitialized) {
+				if (typeof Windows === 'undefined') {
+					// add a listener for the mouse
+					window.removeEventListener('devicemotion', onDeviceMotion, false);
+				} else {
+					// On Windows 8 Devices
+					var accelerometer = Windows.Device.Sensors.Accelerometer.getDefault();
+
+					accelerometer.removeEventListener('readingchanged', onDeviceMotion, false);
+				}
+				accelInitialized = false;
+			}
+		};
+
+		/**
+		 * watch the device orientation event 
+		 * @name watchDeviceOrientation
+		 * @memberOf me.device
+		 * @public
+		 * @function
+		 * @return {boolean} false if not supported by the device
+		 */
+		obj.watchDeviceOrientation = function() {
+			if(me.sys.hasDeviceOrientation && !deviceOrientationInitialized) {
+				window.addEventListener('deviceorientation', onDeviceRotate, false);
+				deviceOrientationInitialized = true;
+			}
+			return false;
+		}
+
+		/**
+		 * unwatch Device orientation event 
+		 * @name unwatchAccelerometer
+		 * @memberOf me.device
+		 * @public
+		 * @function
+		 */
+		obj.unwatchDeviceOrientation = function() {
+			if(deviceOrientationInitialized) {
+				window.removeEventListener('deviceorientation', onDeviceRotate, false);
+				deviceOrientationInitialized = false;
+			}
+		}
+
+		return obj;
+	})();
+})(window);
--- src/input/input.js
+++ src/input/input.js
@@ -454,33 +454,6 @@
 		  ---------------------------------------------*/
 		
 		/**
-		 * Accelerometer information<br>
-		 * properties : x, y, z
-		 * @public
-		 * @enum {number}
-		 * @name accel
-		 * @memberOf me.input
-		 */
-		obj.accel = {
-			x: 0, 
-			y: 0, 
-			z: 0
-		};
-
-		/** 
-		 * Device Orientation. Stores angle in degrees for each axis.
-		 * properties : tiltLeftRight, tiltFrontBack, direction
-		 * @public
-		 * @name orientation
-		 * @memberOf me.input
-		 */
-		obj.orientation = {
-			tiltLeftRight: 0,
-			tiltFrontBack: 0,
-			direction: 0
-		};
-		
-		/**
 		 * Mouse information<br>
 		 * properties : <br>
 		 * pos (me.Vector2d) : pointer position (in screen coordinates) <br>
@@ -921,91 +894,6 @@
 			}
 			throw "melonJS : invalid event type : " + eventType;
 		};
-
-		/**
-		 * watch Accelerator event 
-		 * @name watchAccelerometer
-		 * @memberOf me.input
-		 * @public
-		 * @function
-		 * @return {boolean} false if not supported by the device
-		 */
-		obj.watchAccelerometer = function () {
-		    if (me.sys.hasAccelerometer) {
-		        if (!accelInitialized) {
-		            if (typeof Windows === 'undefined') {
-		                // add a listener for the devicemotion event
-		                window.addEventListener('devicemotion', onDeviceMotion, false);
-		            } else {
-		                // On Windows 8 Device
-		                var accelerometer = Windows.Devices.Sensors.Accelerometer.getDefault();
-		                if (accelerometer) {
-		                    // Capture event at regular intervals
-		                    var minInterval = accelerometer.minimumReportInterval;
-		                    var Interval = minInterval >= 16 ? minInterval : 25;
-		                    accelerometer.reportInterval = Interval;
-
-		                    accelerometer.addEventListener('readingchanged', onDeviceMotion, false);
-		                }
-		            }
-		            accelInitialized = true;
-		        }
-		        return true;
-		    }
-		    return false;
-		};
-		
-		/**
-		 * unwatch Accelerometor event 
-		 * @name unwatchAccelerometer
-		 * @memberOf me.input
-		 * @public
-		 * @function
-		 */
-		obj.unwatchAccelerometer = function() {
-		    if (accelInitialized) {
-		        if (typeof Windows === 'undefined') {
-		            // add a listener for the mouse
-		            window.removeEventListener('devicemotion', onDeviceMotion, false);
-		        } else {
-                    // On Windows 8 Devices
-		            var accelerometer = Windows.Device.Sensors.Accelerometer.getDefault();
-
-		            accelerometer.removeEventListener('readingchanged', onDeviceMotion, false);
-		        }
-		        accelInitialized = false;
-		    }
-		};
-
-		/**
-		 * watch the device orientation event 
-		 * @name watchDeviceOrientation
-		 * @memberOf me.input
-		 * @public
-		 * @function
-		 * @return {boolean} false if not supported by the device
-		 */
-		obj.watchDeviceOrientation = function() {
-			if(me.sys.hasDeviceOrientation && !deviceOrientationInitialized) {
-				window.addEventListener('deviceorientation', onDeviceRotate, false);
-				deviceOrientationInitialized = true;
-			}
-			return false;
-		}
-
-		/**
-		 * unwatch Device orientation event 
-		 * @name unwatchAccelerometer
-		 * @memberOf me.input
-		 * @public
-		 * @function
-		 */
-		obj.unwatchDeviceOrientation = function() {
-			if(deviceOrientationInitialized) {
-				window.removeEventListener('deviceorientation', onDeviceRotate, false);
-				deviceOrientationInitialized = false;
-			}
-		}
 
 	    // return our object
 		return obj;
