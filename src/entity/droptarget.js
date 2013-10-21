a85eeaf5a0856cf86bfb8e2a796a2d755967424e
Jeroen Reurings <j.reurings@gmail.com>
2013-10-21 07:47:29 +0200

    - refactored droptarget into a module

--- src/entity/droptarget.js
+++ src/entity/droptarget.js
@@ -4,92 +4,124 @@
  * http://www.melonjs.org
  * @desc Used to make a game entity a droptarget
  */
-me.DroptargetEntity = (function (Entity, Event, Rect) {
+me.DroptargetEntity = (function (Event, Rectangle) {
     'use strict';
-    return Entity.extend({
-        /**
-         * constant for the overlaps method
-         * @public
-         * @constant
-         * @type String
-         * @name CHECKMETHOD_OVERLAP
-         */
-        CHECKMETHOD_OVERLAP: "overlaps",
-        /**
-         * constant for the contains method
-         * @public
-         * @constant
-         * @type String
-         * @name CHECKMETHOD_CONTAINS
-         */
-        CHECKMETHOD_CONTAINS: "contains",
-        /**
-         * the checkmethod we want to use
-         * @public
-         * @constant
-         * @type String
-         * @name checkMethod
-         */
-        checkMethod: null,
-        /**
-         * Constructor
-         * @name init
-         * @memberOf me.DroptargetEntity
-         * @function
-         * @param {Number} x the x postion of the entity
-         * @param {Number} y the y postion of the entity
-         * @param {Object} settings the additional entity settings
-         */
-        init: function (x, y, settings) {
-            this.parent(x, y, settings);
-            Event.subscribe(Event.DRAGEND, this.checkOnMe.bind(this));
-            this.checkMethod = this[this.CHECKMETHOD_OVERLAP];
-        },
-        /**
-         * Sets the collision method which is going to be used to check a valid drop
-         * @name setCheckMethod
-         * @memberOf me.DroptargetEntity
-         * @function
-         * @param {Constant} checkMethod the checkmethod (defaults to CHECKMETHOD_OVERLAP)
-         */
-        setCheckMethod: function (checkMethod) {
-            //  We can improve this check,
-            //  because now you can use every method in theory
-            if (this[checkMethod] !== undefined) {
-                this.checkMethod = this[checkMethod];
+    // - cross instance private members -
+
+    /**
+     * Constructor
+     * @name init
+     * @memberOf me.DroptargetEntity
+     * @function
+     * @param {Number} x: the x postion of the entity
+     * @param {Number} y: the y postion of the entity
+     * @param {Object} settings: the additional entity settings
+     */
+    return function (obj) {
+        // - per instance private members -
+            /**
+             * the checkmethod we want to use
+             * @public
+             * @constant
+             * @type String
+             * @name checkMethod
+             */
+        var checkMethod = null,
+            /**
+             * Gets called when a draggable entity is dropped on the current entity
+             * @name drop
+             * @memberOf me.DroptargetEntity
+             * @function
+             * @param {Object} draggableEntity: the draggable entity that is dropped
+             */
+            drop = function (draggableEntity) {
+                // could be used to fire default drop logic
+            },
+            /**
+             * Checks if a dropped entity is dropped on the current entity
+             * @name checkOnMe
+             * @memberOf me.DroptargetEntity
+             * @function
+             * @param {Object} e: the drag event
+             * @param {Object} draggableEntity: the draggable entity that is dropped
+             */
+            checkOnMe = function (e, draggableEntity) {
+                // the check if the draggable entity is this entity should work after
+                // a total refactoring to the module pattern
+                if (draggableEntity && draggableEntity !== obj &&
+                    obj[checkMethod](draggableEntity.collisionBox)) {
+                        // call the drop method on the current entity
+                        drop(draggableEntity);
+                        if (obj.drop) {
+                            obj.drop(e);
+                        }
+                }
+            };
+
+        // - external interface -
+        obj.mix({
+            /**
+             * constant for the overlaps method
+             * @public
+             * @constant
+             * @type String
+             * @name CHECKMETHOD_OVERLAPS
+             */
+            CHECKMETHOD_OVERLAPS: "overlaps",
+            /**
+             * constant for the contains method
+             * @public
+             * @constant
+             * @type String
+             * @name CHECKMETHOD_CONTAINS
+             */
+            CHECKMETHOD_CONTAINS: "contains",
+            /**
+             * Sets the collision method which is going to be used to check a valid drop
+             * @name setCheckMethod
+             * @memberOf me.DroptargetEntity
+             * @function
+             * @param {Constant} checkMethod: the checkmethod (defaults to CHECKMETHOD_OVERLAP)
+             */
+            setCheckMethod: function (value) {
+                if (this[value] !== undefined) {
+                    checkMethod = value;
+                }
+            },
+            /**
+             * Destructor
+             * @name destroy
+             * @memberOf me.DroptargetEntity
+             * @function
+             */
+            destroy: function () {
+                Event.unsubscribe(Event.DRAGEND, checkOnMe);
+            },
+            /**
+             * Updates the entity per cycle, can be overwritten
+             * @name update
+             * @memberOf me.DroptargetEntity
+             * @function
+             */
+            update: function () {
+                return true;
+            },
+            /**
+             * Draws the entity per cycle, can be overwritten
+             * @name draw
+             * @memberOf me.DroptargetEntity
+             * @function
+             */
+            draw: function () {
+
             }
-        },
-        /**
-         * Checks if a dropped entity is dropped on the current entity
-         * @name checkOnMe
-         * @memberOf me.DroptargetEntity
-         * @function
-         * @param {Object} draggableEntity the draggable entity that is dropped
-         */
-        checkOnMe: function (e, draggableEntity) {
-            if (draggableEntity && this.checkMethod(draggableEntity.collisionBox)) {
-                // call the drop method on the current entity
-                this.drop(draggableEntity);
-            }
-        },
-        /**
-         * Gets called when a draggable entity is dropped on the current entity
-         * @name drop
-         * @memberOf me.DroptargetEntity
-         * @function
-         * @param {Object} draggableEntity the draggable entity that is dropped
-         */
-        drop: function (draggableEntity) {
-            //  Could be used to fire default drop logic
-        },
-        /**
-         * Destructor
-         * @name destroy
-         * @memberOf me.DroptargetEntity
-         * @function
-         */
-        destroy: function () {
-            Event.unsubscribe(Event.DRAGEND, this.checkOnMe);
-        }
-    });
-}(me.ObjectEntity, me.event, me.Rect));
+        });
+
+        // - initialisation logic -
+        Event.subscribe(Event.DRAGEND, checkOnMe.bind(obj));
+        checkMethod = obj.CHECKMETHOD_OVERLAPS;
+        
+        // - return external interface -
+        return obj;
+    };
+}(me.event, me.Rect));
