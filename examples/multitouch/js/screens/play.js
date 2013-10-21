67e5b7ca885b97c95c977f5983237cb1cd3f60e9
Jeroen Reurings <j.reurings@gmail.com>
2013-10-21 07:49:17 +0200

    - removed new from play screen (it has become obsolete)

--- examples/multitouch/js/screens/play.js
+++ examples/multitouch/js/screens/play.js
@@ -6,8 +6,8 @@ game.PlayScreen = me.ScreenObject.extend({
         // clear the background
         me.game.add(new me.ColorLayer("background", "#000000", 0), 0);
         // add a few squares
-        me.game.add(new game.square(10, 10, {width: 100, height: 100}), 1);
-        me.game.add(new game.square(400, 300, {width: 100, height: 100}), 2);
-        me.game.add(new game.square(750, 600, {width: 100, height: 100}), 1);
+        me.game.add(game.square(10, 10, {width: 100, height: 100}), 1);
+        me.game.add(game.square(400, 300, {width: 100, height: 100}), 2);
+        me.game.add(game.square(750, 600, {width: 100, height: 100}), 1);
     }
 });
