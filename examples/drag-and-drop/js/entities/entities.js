49fbc481790937a52d068f9c8a6f00711dfd62d0
Jeroen Reurings <j.reurings@gmail.com>
2013-10-21 12:45:22 +0200

    - added a third drop target to the drag & drop example which is also draggable

--- examples/drag-and-drop/js/entities/entities.js
+++ examples/drag-and-drop/js/entities/entities.js
@@ -59,11 +59,13 @@ function (BaseEntity, DroptargetEntity) {
             // add the droptarget ability to the mix
             droptarget = base.mix(DroptargetEntity(base)),
             // set the initial color to red
-            color = 'red',
+            startColor = 'red',
+            // set the current color to the start color
+            color = startColor,
             // set the font we want to use
             font = new me.Font('Verdana', 15, 'black'),
             // set the text
-            text = 'Drop on me\n\nAnd I\'ll turn green\n\ncheckmethod: overlap',
+            text = 'Drop on me\n\nAnd I\'ll turn green\n\ncheckmethod: overlaps',
             // mix in some custom methods
             obj = droptarget.mix({
                 draw: function (context) {
@@ -76,18 +78,21 @@ function (BaseEntity, DroptargetEntity) {
                     );
                     font.draw(context, text, this.pos.x, this.pos.y);
                 },
+                // droptarget specific methods
                 drop: function (e) {
                     // indicate a succesful drop, set color to green
                     color = 'green';
-                    // set the color back to red after a second
+                    // set the color back to the start color after a second
                     window.setTimeout(function () {
-                        color = 'red';
+                        color = startColor;
                     }, 1000);
                 },
+                // custom methods
                 setText: function (value) {
                     text = value;
                 },
                 setColor: function (value) {
+                    startColor = value;
                     color = value;
                 }
             });
@@ -111,3 +116,40 @@ game.droptarget2 = function(x, y, settings) {
     // return the droptarget2 entity
     return obj;
 };
+
+game.droptarget3 = me.import([
+    // import our dependency
+    'DraggableEntity'
+],
+// the dependency will be passed to the callback functon
+function (DraggableEntity) {
+    return function(x, y, settings) {
+            // construct a new droptarget2 instance
+        var droptarget2 = game.droptarget2(x, y, settings),
+            // add the draggable ability to the mix
+            draggable = droptarget2.mix(DraggableEntity(droptarget2)),
+            // mix in draggable specific methods
+            obj = draggable.mix({
+                dragStart: function () {
+                    this.setColor('green');
+                },
+                dragMove: function () {
+                    this.setColor('blue');
+                },
+                dragEnd: function () {
+                    this.setColor(startColor);
+                }
+            }),
+            startColor = 'purple';
+
+        // change the text
+        obj.setText('Drop on me\n\nAnd I\'ll turn green\n\ncheckmethod: overlaps\n\n' +
+            'I\'m also draggable');
+        // change the color
+        obj.setColor(startColor);
+        // change the check method
+        obj.setCheckMethod(obj.CHECKMETHOD_OVERLAPS);
+        // return the droptarget3 entity
+        return obj;
+    };
+});
--- examples/drag-and-drop/js/screens/play.js
+++ examples/drag-and-drop/js/screens/play.js
@@ -6,13 +6,17 @@ game.PlayScreen = me.ScreenObject.extend({
         // clear the background
         me.game.add(new me.ColorLayer("background", "#000000", 0), 0);
         // add a few squares
-        me.game.add(game.square(200, 230, {width: 100, height: 100}), 1);
-        me.game.add(game.square(200, 430, {width: 100, height: 100}), 1);
+        me.game.add(game.square(200, 50, {width: 100, height: 100}), 1);
+        me.game.add(game.square(200, 250, {width: 100, height: 100}), 1);
+        me.game.add(game.square(200, 450, {width: 100, height: 100}), 1);
 
         // add a droptarget entity
-        me.game.add(game.droptarget(400, 200, {width: 200, height: 150}), 1);
+        me.game.add(game.droptarget(400, 20, {width: 200, height: 150}), 1);
 
         // add another droptarget entity
-        me.game.add(game.droptarget2(400, 400, {width: 200, height: 150}), 1);
+        me.game.add(game.droptarget2(400, 220, {width: 200, height: 150}), 1);
+
+        // add another droptarget entity
+        me.game.add(game.droptarget3(400, 420, {width: 200, height: 150}), 1);
     }
 });
