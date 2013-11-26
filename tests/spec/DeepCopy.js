bf4f13418cf128badc2f57202f7b30d51bfdac60
Jason Oster <jay@kodewerx.org>
2013-11-26 03:56:45 -0800

    Cleanup Spec format, and add failing test for #307

--- tests/spec/DeepCopy.js
+++ tests/spec/DeepCopy.js
@@ -12,39 +12,52 @@ describe("DeepCopy", function () {
     var vec1 = new myVect1(25, 25);
     var vec2 = new myVect2(50, 50);
 
-    it("is an instance of me.Vector2d", function () {
-        expect(vec1).toBeInstanceOf(me.Vector2d);
-        expect(vec2).toBeInstanceOf(me.Vector2d);
+    describe("vec1", function () {
+        it("is an instance of me.Vector2d", function () {
+            expect(vec1).toBeInstanceOf(me.Vector2d);
+        });
+
+        it("is not an instance of myVect2", function () {
+            expect(vec1).not.toBeInstanceOf(myVect2);
+        });
+
+        it("has an instance of me.Vector2d", function () {
+            expect(vec1.a).toBeInstanceOf(me.Vector2d);
+        });
+
+        it("has an instance of Array", function () {
+            expect(vec1.b).toBeInstanceOf(Array);
+        });
+
+        it("has different object references", function () {
+            expect(vec1.a).not.toBe(vec2.a);
+            expect(vec1.b).not.toBe(vec2.b);
+        });
     });
 
-    it("is not an instance of myVect2", function () {
-        expect(vec1).not.toBeInstanceOf(myVect2);
-    });
+    describe("vec2", function () {
+        it("is an instance of me.Vector2d", function () {
+            expect(vec2).toBeInstanceOf(me.Vector2d);
+        });
 
-    it("is an instance of myVect1", function () {
-        expect(vec2).toBeInstanceOf(myVect1);
-    });
+        it("is an instance of myVect1", function () {
+            expect(vec2).toBeInstanceOf(myVect1);
+        });
 
-    it("has an instance of me.Vector2d", function () {
-        expect(vec1.a).toBeInstanceOf(me.Vector2d);
-        expect(vec2.a).toBeInstanceOf(me.Vector2d);
-    });
+        it("has an instance of me.Vector2d", function () {
+            expect(vec2.a).toBeInstanceOf(me.Vector2d);
+        });
 
-    it("has an instance of Array", function () {
-        expect(vec1.b).toBeInstanceOf(Array);
-        expect(vec2.b).toBeInstanceOf(Array);
-    });
+        it("has an instance of Array", function () {
+            expect(vec2.b).toBeInstanceOf(Array);
+        });
 
-    it("is not the same object", function () {
-        expect(vec1.a).not.toBe(vec2.a);
-        expect(vec1.b).not.toBe(vec2.b);
-    });
+        it("does not effect vec1 when changed", function () {
+            vec2.b[0] = 3;
+            expect(vec1.b[0]).not.toEqual(vec2.b[0]);
 
-    it("does not effect the other when changed", function () {
-        vec2.b[0] = 3;
-        expect(vec1.b[0]).not.toEqual(vec2.b[0]);
-
-        vec2.c.set(1, 0);
-        expect(vec1.c.x).not.toEqual(vec2.c.x);
+            vec2.c.set(1, 0);
+            expect(vec1.c.x).not.toEqual(vec2.c.x);
+        });
     });
 });
