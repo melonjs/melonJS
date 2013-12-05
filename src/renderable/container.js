22cea8a84eebfed9b931905c643eedc3e221b559
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-12-04 19:57:06 -0500

    changed condition to check for visibility when floating

--- src/renderable/container.js
+++ src/renderable/container.js
@@ -606,7 +606,7 @@
 
 			for ( var i = this.children.length, obj; i--, obj = this.children[i];) {
 				isFloating = obj.floating;
-				if ((obj.inViewport || isFloating) && obj.isRenderable) {
+				if ((obj.inViewport || (isFloating && obj.isVisible)) && obj.isRenderable) {
 
 					if (isFloating === true) {
 						context.save();
