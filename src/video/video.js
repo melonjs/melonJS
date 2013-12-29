4d7cbac88d8bb3e458491b9df75c19059d119d6c
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-12-29 14:39:45 -0500

    put save & restore back to clear, due to setTransform call.

--- src/video/video.js
+++ src/video/video.js
@@ -413,10 +413,11 @@
 		 */
 		api.clearSurface = function(context, col) {
 			var _canvas = context.canvas;
-
+			context.save();
 			context.setTransform(1, 0, 0, 1, 0, 0);
 			context.fillStyle = col;
 			context.fillRect(0, 0, _canvas.width, _canvas.height);
+			context.restore();
 		};
 		
 		/**
