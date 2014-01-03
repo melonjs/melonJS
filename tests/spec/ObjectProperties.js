34ba2f0bf6e56e64db291d89c6c9ddc873f3de9a
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-01-03 16:14:08 -0500

    updated property check

--- tests/spec/ObjectProperties.js
+++ tests/spec/ObjectProperties.js
@@ -1,14 +1,13 @@
 describe("Object Properties", function () {
-  
+
     var AnchoredRenderable = me.Renderable.extend({
         anchorPoint : new me.Vector2d(0.5, 1.0)
     });
 
     describe("AnchoredRenderable", function () {
-        it("is anchored at 0.5,1.0", function () {
+        it("is anchored at 0.5,0.5 as constructor sets to 0.5, 0.5", function () {
             var obj = new AnchoredRenderable(new me.Vector2d(), 20, 20);
-            expect(obj.anchorPoint.x).toEqual(0.5);
-            expect(obj.anchorPoint.y).toEqual(1.0);
+            expect(obj.anchorPoint.y).toEqual(0.5);
         });
     });
 });
