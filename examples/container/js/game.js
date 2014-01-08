bc47a740bd014637a756301e4839739892cd382c
obiot <olivier.biot@me.com>
2014-01-08 11:40:45 +0800

    [#356] fixed the defer function call in all examples

--- examples/container/js/game.js
+++ examples/container/js/game.js
@@ -16,7 +16,7 @@ var game = {
         // add "#debug" to the URL to enable the debug Panel
         if (document.location.hash === "#debug") {
             window.onReady(function () {
-                me.plugin.register.defer(debugPanel, "debug");
+                me.plugin.register.defer(this, debugPanel, "debug");
             });
         }
 
