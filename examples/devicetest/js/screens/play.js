ec857e4024785e3d5eaa3c79cb7703a42e946ced
obiot <olivier.biot@me.com>
2014-01-21 11:06:54 +0800

    [#321] updated the device test example (same as before to remove the s

--- examples/devicetest/js/screens/play.js
+++ examples/devicetest/js/screens/play.js
@@ -1,34 +1,41 @@
 game.PlayScreen = me.ScreenObject.extend({
-  init: function() {
-    this.parent(true, true);
-    this.font = new me.Font('arial', '24px', '#fff');
-  },
-  draw: function(ctx) {
-    me.video.clearSurface(ctx, '#000');
-    var o = me.device;
-    this.font.draw(ctx, "Gamma: " + o.gamma, 10, 0);
-    this.font.draw(ctx, "Beta: " + o.beta, 10, 30);
-    this.font.draw(ctx, "Alpha: " + o.alpha, 10, 60);
-    this.font.draw(ctx, "X: " + o.accelerationX, 10, 90);
-    this.font.draw(ctx, "Y: " + o.accelerationY, 10, 120);
-    this.font.draw(ctx, "Z: " + o.accelerationZ, 10, 150);
-    this.font.draw(ctx, "orientation: " + me.device.orientation + " degrees", 10, 180);
-  },
-  onResetEvent: function() {  
-    me.device.watchDeviceOrientation();
-    me.device.watchAccelerometer();
-  },
+    /** 
+     *  action to perform on state change
+     */
+    onResetEvent: function() {  
+    
+        me.device.watchDeviceOrientation();
+        me.device.watchAccelerometer();
+        
+        // clear the background
+        me.game.world.addChild(new me.ColorLayer("background", "#000000", 0));
+        
+        // renderable to display device information
+        me.game.world.addChild(new (me.Renderable.extend({
+            init: function() {
+                this.parent(new me.Vector2d(), 100, 200);
+                this.font = new me.Font('arial', '24px', '#fff');
+            },
+            update: function() {
+                return true;
+            },
+            draw: function(context) {
+                this.font.draw(context, "Gamma: " + me.device.gamma, 10, 0);
+                this.font.draw(context, "Beta: " + me.device.beta, 10, 30);
+                this.font.draw(context, "Alpha: " + me.device.alpha, 10, 60);
+                this.font.draw(context, "X: " + me.device.accelerationX, 10, 90);
+                this.font.draw(context, "Y: " + me.device.accelerationY, 10, 120);
+                this.font.draw(context, "Z: " + me.device.accelerationZ, 10, 150);
+                this.font.draw(context, "orientation: " + me.device.orientation + " degrees", 10, 180);
+            }
+        })), 1);
+    },
   
-  
-  /**  
-   *  action to perform when leaving this screen (state change)
-   */
-  onDestroyEvent: function() {
-    me.device.unwatchDeviceOrientation();
-    me.device.unwatchAccelerometer();
-  },
-
-  update: function() {
-    return true;
-  }
+    /**  
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        me.device.unwatchDeviceOrientation();
+        me.device.unwatchAccelerometer();
+    }
 });
