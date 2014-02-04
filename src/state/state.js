9ccdb00e59c89ae2f25e12309c0b7c624c66a525
Jason Oster <jay@kodewerx.org>
2014-02-03 16:37:47 -0800

    Fix for documentation

--- src/state/state.js
+++ src/state/state.js
@@ -707,7 +707,7 @@
 		 * @memberOf me.state
 		 * @public
 		 * @function
-		 * @param {Boolean} true if a "process is running"
+		 * @return {Boolean} true if a "process is running"
 		 */
 		obj.isRunning = function() {
 			return _animFrameId !== -1;
@@ -719,7 +719,7 @@
 		 * @memberOf me.state
 		 * @public
 		 * @function
-		 * @param {Boolean} true if the game is paused
+		 * @return {Boolean} true if the game is paused
 		 */
 		obj.isPaused = function() {
 			return _isPaused;
