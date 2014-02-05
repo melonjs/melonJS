6e057afc41ba7cb550d9dae51d64bca7786f9dd3
Olivier Biot <olivier.biot@me.com>
2014-02-05 12:08:29 +0800

    Check first for `offsetWidth/offsetHeight` if the canvas parent `width/height` properties are undefined
    @see https://groups.google.com/forum/#!topic/melonjs/SVVuYqjhSZ8

--- src/video/video.js
+++ src/video/video.js
@@ -327,8 +327,10 @@
 			if (auto_scale) {
 				// get the parent container max size
 				var parent = me.video.getScreenCanvas().parentNode;
-				var _max_width = Math.min(maxWidth, parent.width || window.innerWidth);
-				var _max_height = Math.min(maxHeight, parent.height || window.innerHeight);
+				// getBoudindClientRect function should be use here too
+				// https://developer.mozilla.org/en-US/docs/Determining_the_dimensions_of_elements
+				var _max_width = Math.min(maxWidth, parent.width || parent.offsetWidth || window.innerWidth);
+				var _max_height = Math.min(maxHeight, parent.height || parent.offsetHeight || window.innerHeight);
 
 				if (maintainAspectRatio) {
 					// make sure we maintain the original aspect ratio
