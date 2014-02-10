4f2b8cf720de60f74c342642f2714c35e6b8a909
obiot <olivier.biot@me.com>
2014-02-10 14:21:35 +0800

    [tests] added a Jasmine Unit test for `me.Rect`

---
+++ tests/spec/Rectangle.js
@@ -0,0 +1,101 @@
+describe("Shape : me.Rect", function () {
+
+    // rect 2 overlap rect 1
+    // rect 3 contains rect 1 and rect 2
+    // rect 4 does not overlap any rectangle
+    // rect 5 is the merge of rect 3 and
+    var rect1 = new me.Rect(new me.Vector2d(0,0), 100, 100);
+    var rect2 = new me.Rect(new me.Vector2d(50,50), 100, 100);
+    var rect3 = new me.Rect(new me.Vector2d(0,0), 150, 150);
+    var rect4 = new me.Rect(new me.Vector2d(500,500), 50, 50);
+    // merge rect 3 and rect 4
+    var rect5 = rect2.clone().union(rect4);
+    
+    console.log(rect5);
+
+    describe("rect1", function () {
+        it("rect 1 overlaps rect2", function () {
+            expect(rect1.overlaps(rect2)).toEqual(true);
+        });
+
+        it("rect 1 overlaps rect3", function () {
+             expect(rect1.overlaps(rect3)).toEqual(true);
+        });
+        
+        it("rect 1 within rect3", function () {
+            expect(rect1.within(rect3)).toEqual(true);
+        });
+
+        it("rect 1 does not overlaps rect4", function () {
+             expect(rect1.overlaps(rect4)).toEqual(false);
+        });
+    });
+    
+    describe("rect2", function () {
+        it("rect 2 within rect3", function () {
+            expect(rect2.within(rect3)).toEqual(true);
+        });
+
+        it("rect 2 overlaps rect3", function () {
+             expect(rect1.overlaps(rect3)).toEqual(true);
+        });
+
+        it("rect 2 does not overlaps rect4", function () {
+             expect(rect1.overlaps(rect4)).toEqual(false);
+        });
+        
+        it("rect 2 does not equals rect4", function () {
+             expect(rect2.equals(rect4)).toEqual(false);
+        });
+    });
+    
+    describe("rect3", function () {
+        it("rect 3 contains rect1", function () {
+             expect(rect3.contains(rect1)).toEqual(true);
+        });
+        
+        it("rect 3 contains rect2", function () {
+             expect(rect3.contains(rect2)).toEqual(true);
+        });
+        
+        it("rect 3 contains the point (70, 150)", function () {
+             expect(rect3.containsPoint(70, 150)).toEqual(true);
+        });
+        
+        it("rect 3 does not overlaps rect4", function () {
+             expect(rect3.overlaps(rect4)).toEqual(false);
+        });
+    });
+    
+    describe("rect 5", function () {
+        it("rect 5 width is 500", function () {
+             expect(rect5.width).toEqual(500);
+        }); 
+        
+        it("rect 5 height is 500", function () {
+             expect(rect5.width).toEqual(500);
+        }); 
+        
+        it("rect 5 pos is (50,50)", function () {
+             expect(rect5.pos.equals({x:50,y:50})).toEqual(true);
+        }); 
+        
+        it("rect 5 overlaps rect1", function () {
+             expect(rect5.overlaps(rect1)).toEqual(true);
+        });
+        
+        it("rect 5 contains rect2", function () {
+             expect(rect5.contains(rect2)).toEqual(true);
+        });
+           
+        it("rect 5 overlaps rect3", function () {
+             expect(rect5.overlaps(rect3)).toEqual(true);
+        });
+        
+        it("rect 5 contains rect4", function () {
+             expect(rect5.contains(rect4)).toEqual(true);
+        });
+               
+    });
+
+});
