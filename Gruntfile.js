50f3ec8c273f9c6bfa8694526e62048d7d831092
obiot <olivier.biot@me.com>
2014-02-14 11:24:27 +0800

    [documentation] now relies on a proper grunt task, instead of using a local hard-copy
    don't forget to run first `npm install` to update all dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,6 +155,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-replace');
 
     // Custom Tasks
