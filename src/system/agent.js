79ec038c7a44cc264a2435c52cb4b36d52b28a37
Jason Oster <jay@kodewerx.org>
2014-02-19 02:30:40 -0800

    Add missing file(!)

---
+++ src/system/agent.js
@@ -0,0 +1,81 @@
+/*
+ * MelonJS Game Engine
+ * Copyright (C) 2011 - 2014, Olivier Biot, Jason Oster
+ * http://www.melonjs.org/
+ *
+ */
+
+(function(window) {
+
+    /**
+     * A collection of utilities to ease porting between different user agents.
+     * @namespace me.agent
+     * @memberOf me
+     */
+    me.agent = (function() {
+        var api = {};
+
+        /**
+         * Known agent vendors
+         * @ignore
+         */
+        var vendors = [ "ms", "MS", "moz", "webkit", "o" ];
+
+        /**
+         * Get a vendor-prefixed property
+         * @public
+         * @name prefixed
+         * @function
+         * @param {String} name Property name
+         * @param {Object} [obj=window] Object or element reference to access
+         * @return {Mixed} Value of property
+         * @memberOf me.agent
+         */
+        api.prefixed = function (name, obj) {
+            obj = obj || window;
+            if (name in obj) return obj[name];
+
+            var uc_name = me.utils.wordUpperCase(name);
+
+            var result;
+            vendors.some(function (vendor) {
+                var name = vendor + uc_name;
+                return result = (name in obj) ? obj[name] : undefined;
+            });
+            return result;
+        };
+
+        /**
+         * Set a vendor-prefixed property
+         * @public
+         * @name setPrefixed
+         * @function
+         * @param {String} name Property name
+         * @param {Mixed} value Property value
+         * @param {Object} [obj=window] Object or element reference to access
+         * @memberOf me.agent
+         */
+        api.setPrefixed = function (name, value, obj) {
+            obj = obj || window;
+            if (name in obj) {
+                obj[name] = value;
+                return;
+            }
+
+            var uc_name = me.utils.wordUpperCase(name);
+
+            vendors.some(function (vendor) {
+                var name = vendor + uc_name;
+                if (name in obj) {
+                    obj[name] = value;
+                    return true;
+                }
+                return false;
+            });
+        };
+
+        return api;
+    })();
+
+
+})(window);
