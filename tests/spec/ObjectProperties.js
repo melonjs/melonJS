4ce6554d0fff092578bdd0b19a8020f4acd9b2fc
Ellison Leão <ellisonleao@gmail.com>
2014-05-26 19:31:20 -0300

    Fix lint tests tests/spec/ObjectProperties.js

--- tests/spec/ObjectProperties.js
+++ tests/spec/ObjectProperties.js
@@ -1,10 +1,10 @@
 describe("Object Properties", function () {
     describe("AnchoredRenderable", function () {
         it("throws an exception, because properties cannot be defined on the prototype", function () {
-            expect(function() {
+            expect(function () {
                 me.Renderable.extend({
                     anchorPoint : new me.Vector2d(0.5, 1.0)
-                })
+                });
             }).toThrow();
         });
     });
