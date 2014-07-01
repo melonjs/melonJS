6a2a3745f982e282d92afc55989d0cd8c8f26c47
Aaron McLeod <sircoolguy99@gmail.com>
2014-07-01 12:31:19 -0400

    refactored base logic into a canvas renderer

--- src/video/video.js
+++ src/video/video.js
@@ -17,17 +17,12 @@
 
         // internal variables
         var canvas = null;
-        var context2D = null;
-        var webGLRenderer = null;
-        var backBufferCanvas = null;
-        var backBufferContext2D = null;
+        var renderer = null;
         var wrapper = null;
 
         var deferResizeId = -1;
 
         var double_buffering = false;
-        var game_width_zoom = 0;
-        var game_height_zoom = 0;
         var auto_scale = false;
         var maintainAspectRatio = true;
 
@@ -94,8 +89,8 @@
             }
 
             // default scaled size value
-            game_width_zoom = game_width * me.sys.scale.x;
-            game_height_zoom = game_height * me.sys.scale.y;
+            var game_width_zoom = game_width * me.sys.scale.x;
+            var game_height_zoom = game_height * me.sys.scale.y;
 
             //add a channel for the onresize/onorientationchange event
             window.addEventListener(
@@ -152,29 +147,19 @@
                 canvas.style.height = (canvas.height / me.device.getPixelRatio()) + "px";
             }
 
-            // create the back buffer if we use double buffering
-            if (double_buffering && renderType === me.video.CANVAS) {
-                backBufferCanvas = api.createCanvas(game_width, game_height, false);
-                backBufferContext2D = api.getContext2d(backBufferCanvas);
-            }
-            else {
-                backBufferCanvas = canvas;
-                backBufferContext2D = context2D;
-            }
-
             // set max the canvas max size if CSS values are defined
             if (window.getComputedStyle) {
                 var style = window.getComputedStyle(canvas, null);
                 me.video.setMaxSize(parseInt(style.maxWidth, 10), parseInt(style.maxHeight, 10));
             }
 
-            switch (renderType) {
+            switch (rendererType) {
                 case me.video.CANVAS:
                     // get the 2D context
-                    context2D = api.getContext2d(canvas);
+                    renderer = new me.CanvasRenderer(canvas, double_buffering);
                     break;
                 case me.video.WEBGL:
-                    webGLRenderer = me.webGLRenderer.init();
+                    renderer = new me.WebGLRenderer(canvas.width, canvas.height, canvas);
                     break;
             }
 
@@ -205,8 +190,7 @@
          * @return {Number}
          */
         api.getWidth = function () {
-            return backBufferCanvas.width;
-
+            return canvas.width;
         };
 
         /**
@@ -233,7 +217,7 @@
          * @return {Number}
          */
         api.getHeight = function () {
-            return backBufferCanvas.height;
+            return canvas.height;
         };
 
         /**
@@ -273,37 +257,10 @@
                 _canvas.screencanvas = true;
             }
 
-            _canvas.width = width || backBufferCanvas.width;
-            _canvas.height = height || backBufferCanvas.height;
+            _canvas.width = width || canvas.width;
+            _canvas.height = height || canvas.height;
 
             return _canvas;
-        };
-
-        /**
-         * Returns the 2D Context object of the given Canvas
-         * `getContext2d` will also enable/disable antialiasing features based on global settings.
-         * @name getContext2D
-         * @memberOf me.video
-         * @function
-         * @param {Canvas}
-         * @return {Context2d}
-         */
-        api.getContext2d = function (canvas) {
-            var _context;
-            if (navigator.isCocoonJS) {
-                // cocoonJS specific extension
-                _context = canvas.getContext("2d", {
-                    "antialias" : me.sys.scalingInterpolation
-                });
-            }
-            else {
-                _context = canvas.getContext("2d");
-            }
-            if (!_context.canvas) {
-                _context.canvas = canvas;
-            }
-            me.video.setImageSmoothing(_context, me.sys.scalingInterpolation);
-            return _context;
         };
 
         /**
@@ -322,37 +279,38 @@
         };
 
         /**
-         * return a reference to the screen canvas corresponding 2d Context<br>
+         * return a reference to the screen canvas corresponding 2d or WebGL Context<br>
          * (will return buffered context if double buffering is enabled, or a reference to the Screen Context)
          * @name getScreenContext
          * @memberOf me.video
          * @function
-         * @return {Context2d}
+         * @return {RendererContext}
          */
         api.getScreenContext = function () {
-            return context2D;
+            return renderer.getScreenContext();
         };
 
         /**
-         * return a reference to the system canvas
+         * return a reference to the system canvas for the current renderer
          * @name getSystemCanvas
          * @memberOf me.video
          * @function
          * @return {Canvas}
          */
         api.getSystemCanvas = function () {
-            return backBufferCanvas;
+            return renderer.getSystemCanvas();
         };
 
         /**
-         * return a reference to the system 2d Context
+         * return a reference to the system Context for the current renderer
+         * Returns the GL context if the renderer is webgl.
          * @name getSystemContext
          * @memberOf me.video
          * @function
-         * @return {Context2d}
+         * @return {RendererContext}
          */
         api.getSystemContext = function () {
-            return backBufferContext2D;
+            return renderer.getSystemContext();
         };
 
         /**
@@ -409,7 +367,7 @@
                         // cancel any previous pending resize
                         clearTimeout(deferResizeId);
                     }
-                    deferResizeId = me.video.updateDisplaySize.defer(this, scaleX, scaleY);
+                    deferResizeId = renderer.updateDisplaySize.defer(this, scaleX, scaleY);
                     return;
                 }
             }
@@ -428,37 +386,11 @@
         api.updateDisplaySize = function (scaleX, scaleY) {
             // update the global scale variable
             me.sys.scale.set(scaleX, scaleY);
-
-            var width = game_width_zoom = backBufferCanvas.width * scaleX;
-            var height = game_height_zoom = backBufferCanvas.height * scaleY;
-
-            if (webGLRenderer === null) {
-                // apply the new value
-                canvas.width = width
-                canvas.height = height;
-                
-                // adjust CSS style for High-DPI devices
-                if (me.device.getPixelRatio() > 1) {
-                    canvas.style.width = (canvas.width / me.device.getPixelRatio()) + "px";
-                    canvas.style.height = (canvas.height / me.device.getPixelRatio()) + "px";
-                }
-                me.video.setImageSmoothing(context2D, me.sys.scalingInterpolation);
-            }
-            else {
-                me.webGLRenderer.context.resize(width, height);
-                // adjust CSS style for High-DPI devices
-                if (me.device.getPixelRatio() > 1) {
-                    canvas.style.width = (canvas.width / me.device.getPixelRatio()) + "px";
-                    canvas.style.height = (canvas.height / me.device.getPixelRatio()) + "px";
-                }
-            }
             
-
-            // make sure we have the correct relative canvas position cached
             me.input._offset = me.video.getPos();
 
-            // force a canvas repaint
-            api.blitSurface();
+            // renderer resize logic
+            renderer.resize(scaleX, scaleY);
 
             // clear the timeout id
             deferResizeId = -1;
@@ -469,16 +401,10 @@
          * @name clearSurface
          * @memberOf me.video
          * @function
-         * @param {Context2d} context Canvas context
-         * @param {String} color a CSS color string
+         * @param {String} color a CSS color string, hex format. eg: #ff0000
          */
-        api.clearSurface = function (context, col) {
-            var _canvas = context.canvas;
-            context.save();
-            context.setTransform(1, 0, 0, 1, 0, 0);
-            context.fillStyle = col;
-            context.fillRect(0, 0, _canvas.width, _canvas.height);
-            context.restore();
+        api.clearSurface = function (col) {
+            renderer.clearSurface(col);
         };
 
         /**
@@ -495,46 +421,20 @@
         };
 
         /**
-         * enable/disable Alpha for the specified context
+         * enable/disable Alpha. Only applies to canvas renderer
          * @name setAlpha
          * @memberOf me.video
          * @function
-         * @param {Context2d} context
          * @param {Boolean} enable
          */
-        api.setAlpha = function (context, enable) {
-            context.globalCompositeOperation = enable ? "source-over" : "copy";
+        api.setAlpha = function (enable) {
+            if (typeof renderer.setAlpha === "function") {
+                renderer.setAlpha(enable);
+            }
         };
 
         /**
-         * render the main framebuffer on screen
-         * @name blitSurface
-         * @memberOf me.video
-         * @function
-         */
-        api.blitSurface = function () {
-            if (double_buffering) {
-                /** @ignore */
-                api.blitSurface = function () {
-                    //FPS.update();
-                    context2D.drawImage(
-                        backBufferCanvas, 0, 0,
-                        backBufferCanvas.width, backBufferCanvas.height, 0,
-                        0, game_width_zoom, game_height_zoom
-                    );
-
-                };
-            }
-            else {
-                // "empty" function, as we directly render stuff on "context2D"
-                /** @ignore */
-                api.blitSurface = function () {
-                };
-            }
-            api.blitSurface();
-        };
-
-        /**
+         * this currently only works with the Canvas Renderer
          * apply the specified filter to the main canvas
          * and return a new canvas object with the modified output<br>
          * (!) Due to the internal usage of getImageData to manipulate pixels,
@@ -548,60 +448,7 @@
          * @return {Context2d} context object
          */
         api.applyRGBFilter = function (object, effect, option) {
-            //create a output canvas using the given canvas or image size
-            var _context = api.getContext2d(api.createCanvas(object.width, object.height, false));
-            // get the pixels array of the give parameter
-            var imgpix = me.utils.getPixels(object);
-            // pointer to the pixels data
-            var pix = imgpix.data;
-
-            // apply selected effect
-            var i, n;
-            switch (effect) {
-                case "b&w":
-                    for (i = 0, n = pix.length; i < n; i += 4) {
-                        var grayscale = (3 * pix[i] + 4 * pix[i + 1] + pix[i + 2]) >>> 3;
-                        pix[i] = grayscale; // red
-                        pix[i + 1] = grayscale; // green
-                        pix[i + 2] = grayscale; // blue
-                    }
-                    break;
-
-                case "brightness":
-                    // make sure it's between 0.0 and 1.0
-                    var brightness = Math.abs(option).clamp(0.0, 1.0);
-                    for (i = 0, n = pix.length; i < n; i += 4) {
-
-                        pix[i] *= brightness; // red
-                        pix[i + 1] *= brightness; // green
-                        pix[i + 2] *= brightness; // blue
-                    }
-                    break;
-
-                case "transparent":
-                    var refColor = me.pool.pull("me.Color").parseHex(option);
-                    var pixel = me.pool.pull("me.Color");
-                    for (i = 0, n = pix.length; i < n; i += 4) {
-                        pixel.setColor(pix[i], pix[i + 1], pix[i + 2]);
-                        if (pixel.equals(refColor)) {
-                            pix[i + 3] = 0;
-                        }
-                    }
-                    me.pool.push(refColor);
-                    me.pool.push(pixel);
-
-                    break;
-
-
-                default:
-                    return null;
-            }
-
-            // put our modified image back in the new filtered canvas
-            _context.putImageData(imgpix, 0, 0);
-
-            // return it
-            return _context;
+            return renderer.applyRGBFilter(object, effect, option);
         };
 
         // return our api
