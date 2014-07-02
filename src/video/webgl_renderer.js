1772b0eb4093c263c05123c7ec38a3005202ad0e
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-07-02 08:24:13 -0400

    refactored to use me.Color

--- src/video/webgl_renderer.js
+++ src/video/webgl_renderer.js
@@ -6,35 +6,26 @@
         WebGLContext = null,
         gl = null;
 
-        function hexToR(h) {
-            return parseInt((cutHex(h)).substring(0, 2), 16);
-        }
-
-        function hexToG(h) {
-            return parseInt((cutHex(h)).substring(2, 4), 16);
-        }
-
-        function hexToB(h) {
-            return parseInt((cutHex(h)).substring(4, 6), 16);
-        }
-
-        function cutHex(h) {
-            return (h.charAt(0) === "#") ? h.substring(1, 7) : h;
-        }
-
         api.init = function (width, height, c) {
             canvas = c;
             WebGLContext = require("kami").WebGLContext;
             this.context = new WebGLContext(width, height, canvas);
             gl = this.context.gl;
+            this.color = new me.Color();
         };
 
         api.clearSurface = function (col) {
-            var R = hexToR(col);
-            var G = hexToG(col);
-            var B = hexToB(col);
+            if (col.match(/^\#/)) {
+                color.parseHex(col);
+            }
+            else if (col.match(/^rgb/)) {
+                color.parseRGB(col);
+            }
+            else {
+                color.parseCSS(col);
+            }
 
-            gl.clearColor(R / 255.0, G / 255.0, B / 255.0, 1.0);
+            gl.clearColor(color.r / 255.0, color.g / 255.0, color.b / 255.0, 1.0);
         };
 
         api.resize = function (scaleX, scaleY) {
