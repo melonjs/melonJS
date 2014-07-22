bf7cd412885248ccb31560e9abbdc8af2aa8a78e
Ellison Leão <ellisonleao@gmail.com>
2014-07-22 01:46:02 -0300

    Fix some lint errors / change some task logic

--- tasks/release.js
+++ tasks/release.js
@@ -1,80 +1,81 @@
-'use strict';
-var path = require('path');
-var Q = require('q');
-var shell = require('shelljs');
+"use strict";
+var path = require("path");
+var Q = require("q");
+var shell = require("shelljs");
 
-module.exports = function(grunt) {
-    grunt.registerTask('dorelease', 'MelonJS Release', function() {
-        var repo = path.join(__dirname, '..');
-        var config = grunt.file.readJSON(path.join(repo, 'package.json'));
-        var buildPath = path.join(__dirname, '..', 'build');
+module.exports = function (grunt) {
+    grunt.registerTask("dorelease", "MelonJS Release", function () {
+        var repo = path.join(__dirname, "..");
+        var config = grunt.file.readJSON(path.join(repo, "package.json"));
+        var buildPath = path.join(__dirname, "..", "build");
         var version = config.version;
         var done = this.async();
 
-        // using Q for promises. Using the grunt-release project's same idea
+        function run(cmd, msg) {
+            var deferred = Q.defer();
+            grunt.verbose.writeln("Running: " + cmd);
+            var success = shell.exec(cmd, {silent: true}).code === 0;
+
+            if (success) {
+                grunt.log.ok(msg || cmd);
+                deferred.resolve();
+            } else {
+                // fail and stop execution of further tasks
+                deferred.reject("Failed when executing: `" + cmd + "`\n");
+            }
+            return deferred.promise;
+        }
+
+        function checkout() {
+            run("git checkout --detach", "Detaching from current tree");
+        }
+
+        function add() {
+            grunt.log.oklns("ACTUAL VERSION ==> " + config.version);
+            grunt.log.oklns("BUILD FILES");
+            var filenames = [
+                path.join(buildPath, "melonjs-" + version + ".js"),
+                path.join(buildPath, "melonjs-" + version + "-min.js")
+            ];
+            // check the build files from the actual version
+            // and add the js files to be commited
+            var stringFiles = "";
+            for (var i = 0; i < filenames.length; i++) {
+                if (!grunt.file.exists(filenames[i])) {
+                    grunt.fail.fatal("Missing file " + filenames[i] + " is required");
+                }
+                stringFiles += filenames[i] + " ";
+            }
+            run("git add -f " + stringFiles, "Adding build files");
+        }
+
+        function commit() {
+            run("git commit -am 'Release " + version + " '", "Commiting release");
+        }
+
+        function tag() {
+            run("git tag " + version, "Tagging new version");
+        }
+
+        function push() {
+            run("git push origin " + version, "Pushing to new version branch");
+        }
+
+        function rollback() {
+            run("git checkout master", "Getting back to master branch");
+        }
+        // using Q for promises. Using the grunt-release project"s same idea
         Q()
           .then(checkout)
           .then(add)
           .then(commit)
           .then(tag)
           .then(push)
-          .then(back)
-          .catch(function(msg){
-            grunt.fail.warn(msg || 'release failed')
-          })
-          .finally(done);
+          .then(rollback)
+          .catch(function (msg) {
+            grunt.fail.warn(msg || "release failed");
+        }).finally(done);
 
-        function run(cmd, msg){
-            var deferred = Q.defer();
-            grunt.verbose.writeln('Running: ' + cmd);
-            var success = shell.exec(cmd, {silent:true}).code === 0;
 
-            if (success){
-                grunt.log.ok(msg || cmd);
-                deferred.resolve();
-            }else{
-                // fail and stop execution of further tasks
-                deferred.reject('Failed when executing: `' + cmd + '`\n');
-            }
-            return deferred.promise;
-        }
-
-        function checkout() {
-            run('git checkout --detach', 'Detaching from current tree');
-        }
-
-        function add() {
-            grunt.log.oklns('ACTUAL VERSION ==> ' + config.version);
-            grunt.log.oklns('BUILD FILES');
-            var filenames = [
-                path.join(buildPath, 'melonjs-' + version + '.js'),
-                path.join(buildPath, 'melonjs-' + version + '-min.js')
-            ];
-            // check the build files from the actual version
-            // and add the js files to be commited
-            var stringFiles = '';
-            for (var i = 0; i < filenames.length; i++) {
-                if (grunt.file.exists(filenames[i])) {
-                    stringFiles += filenames[i] + ' ';
-                }
-            }
-            run('git add -f ' + stringFiles, 'Adding build files');
-        }
-
-        function commit() {
-            run('git commit -am "Release '+ version  +'"', 'Commiting release');
-        }
-
-        function tag() {
-            run('git tag ' + version, 'Tagging new version');
-        }
-
-        function push() {
-            run('git push origin master', 'Pushing to master branch');
-        }
-
-        function back() {
-            run('git checkout master', 'Getting back to master branch');
-        }
     });
-}
+};
