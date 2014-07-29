c54b1a3f1c84e3d9904f86cd3b691cd69cb41904
obiot <olivier.biot@me.com>
2014-07-29 10:53:06 +0800

    added automated test units for shape objects
    now that this is working properly and now also a key object in the
    engine, this will ensure we don't break them in the future :)

---
+++ tests/spec/ellipse-spec.js
@@ -0,0 +1,46 @@
+describe("Shape : me.Ellipse", function () {
+
+    // define a ellipse object
+    var circle = new me.Ellipse(50, 50, 100, 100);
+    
+    // get the polyshape bounding rect
+    var boundingRect = circle.getBounds();
+
+    describe("Ellipse ", function () {
+        it("Ellipse x radius is 50", function () {
+            expect(circle.radius.x).toEqual(50);
+        });
+
+        it("Ellipse y radius is 50", function () {
+            expect(circle.radius.y).toEqual(50);
+        });
+
+        it("Ellipse pos is (50, 50)", function () {
+            expect(circle.pos.equals({x: 50, y: 50})).toEqual(true);
+        });
+        
+        it("Ellipse contains the point (75, 75)", function () {
+            expect(circle.containsPoint(75, 75)).toEqual(true);
+        });
+
+        it("Ellipse does not contains the point (75, 500)", function () {
+            expect(circle.containsPoint(75, 500)).toEqual(false);
+        });
+        
+    });
+    
+    describe("Ellipse Bounding Rect", function () {
+        it("Ellipse Bounding Rect width is 100", function () {
+            expect(boundingRect.width).toEqual(100);
+        });
+
+        it("Ellipse Bounding Rect height is 100", function () {
+            expect(boundingRect.height).toEqual(100);
+        });
+
+        it("Ellipse Bounding Rect pos is (0,0)", function () {
+            expect(boundingRect.pos.equals({x: 0, y: 0})).toEqual(true);
+        });
+    });
+
+});
