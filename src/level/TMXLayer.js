75ad1e2918a2fc5349c4ffc22327a73e002098b4
obiot <olivier.biot@me.com>
2014-07-29 16:20:11 +0800

    [#370] fixed the background color drawing when the level is smaller than the display canvas

--- src/level/TMXLayer.js
+++ src/level/TMXLayer.js
@@ -25,6 +25,7 @@
             this.name = name;
             this.color = color;
             this.z = z;
+            this.floating = true;
         },
 
         /**
@@ -35,8 +36,14 @@
             // set layer opacity
             var _alpha = renderer.globalAlpha();
             renderer.setGlobalAlpha(_alpha * this.getOpacity());
-
-            renderer.fillRect(rect.left, rect.top, rect.width, rect.height, this.color);
+            
+            var vpos = me.game.viewport.pos;
+            
+            renderer.fillRect(
+                rect.left - vpos.x, rect.top - vpos.y,
+                rect.width, rect.height,
+                this.color
+            );
 
             // restore context alpha value
             renderer.setGlobalAlpha(_alpha);
