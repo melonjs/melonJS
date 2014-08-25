b90ea5cd02176d74c8e87d731f1b37fcf8897f07
Ellison Leão <ellisonleao@gmail.com>
2014-08-25 00:46:34 -0300

    Rollback changes and use shelljs

--- tasks/release.js
+++ tasks/release.js
@@ -1,7 +1,6 @@
 "use strict";
 var path = require("path");
-var exec = require("child_process").exec;
-var async = require("async");
+var shell = require("shelljs");
 
 module.exports = function (grunt) {
     grunt.registerTask("dorelease", "MelonJS Release", function () {
@@ -10,156 +9,90 @@ module.exports = function (grunt) {
         var version = config.version;
         var currBranch;
         var verbose = grunt.option("verbose");
-
-        function run(cmd, msg, callback) {
-            grunt.log.oklns("Running: " + msg);
-            exec(cmd, callback);
+        // we could add more options here later
+        var shellOpts = {};
+        if (verbose) {
+            shellOpts['verbose'] = true;
         }
 
-        async.series([
-            function checkout(callback) {
-                var symbolicRef;
-                var msg = "Detaching from current tree";
-                run("git symbolic-ref HEAD", msg,
-                    function(error, stdout, stderr) {
-                        grunt.log.writeln("STDOUT: " + stdout);
-                        if (error) {
-                            if (verbose) {
-                                grunt.log.error("Error: " + error);
-                                grunt.log.error("STDERR: " + stderr);
-                            }
-                            callback(true, "Could not get the actual branch from symbolic ref");
-                        }
-                        symbolicRef = stdout;
-                    }
-                );
-                var splitted = symbolicRef.split("/");
-                currBranch = splitted.slice(2).join("/");
-                if (verbose) {
-                    grunt.log.writeln("Current branch: " + currBranch);
-                }
-                if (!currBranch) {
-                    callback(true, "Could not get the actual branch from symbolic ref");
-                }
-                run("git checkout --detach", msg,
-                    function(error, stdout, stderr) {
-                        grunt.log.writeln("STDOUT: " + stdout);
-                        if (error) {
-                            if (verbose) {
-                                grunt.log.error("Error: " + error);
-                                grunt.log.error("STDERR: " + stderr);
-                            }
-                            callback(true, "Git detach error");
-                        }
-                    }
-                );
-                grunt.log.ok();
-                callback(null, msg);
-            },
-            function add(callback) {
-                var msg = "Adding build files";
-                grunt.log.oklns("ACTUAL VERSION ==> " + config.version);
-                grunt.log.oklns("BUILD FILES");
-                var filenames = [
-                    grunt.config.get("path.main"),
-                    grunt.config.get("path.min")
-                ];
-                // check the build files from the actual version
-                // and add the js files to be commited
-                var stringFiles = "";
-                for (var i = 0; i < filenames.length; i++) {
-                    if (!grunt.file.exists(filenames[i])) {
-                        grunt.fail.fatal("Missing file " + filenames[i] + " is required");
-                    }
-                    if (verbose) {
-                        grunt.log.oklns(filenames[i] + " exists!");
-                    }
-                    stringFiles += filenames[i] + " ";
-                }
-                run("git add -f " + stringFiles, msg,
-                    function(error, stdout, stderr) {
-                        grunt.log.writeln("STDOUT: " + stdout);
-                        if (error) {
-                            if (verbose) {
-                                grunt.log.error("Error: " + error);
-                                grunt.log.error("STDERR: " + stderr);
-                            }
-                            callback(true, "Git add error");
-                        }
-                    }
-                );
-                grunt.log.ok();
-                callback(null, msg);
-            },
-            function commit(callback) {
-                var msg = "Commiting release";
-                run("git commit -am 'Release " + version + "'", msg, function(error, stdout, stderr) {
-                    grunt.log.writeln("STDOUT: " + stdout);
-                    if (error) {
-                        if (verbose) {
-                            grunt.log.error("Error: " + error);
-                            grunt.log.error("STDERR: " + stderr);
-                        }
-                        callback(true, "Git release commit error");
-                    }
+        function run(cmd, msg) {
+            grunt.verbose.writeln("Running: " + cmd);
+            var success = shell.exec(cmd, shellOpts).code === 0;
 
-                });
-                grunt.log.ok();
-                callback(null, msg);
-            },
-            function tag(callback) {
-                var msg = "Tagging new version";
-                run("git tag -a "+ version +" -m 'melonJS "+ version +" version'", msg, function(error, stdout, stderr) {
-                    grunt.log.writeln("STDOUT: " + stdout);
-                    if (error) {
-                        if (verbose) {
-                            grunt.log.error("Error: " + error);
-                            grunt.log.error("STDERR: " + stderr);
-                        }
-                        callback(true, "Git tag error");
-                    }
-                });
-                grunt.log.ok();
-                callback(null, msg);
-            },
-            function push(callback) {
-                var msg = "Pushing to new version branch";
-                run("git push origin " + version, msg, function(error, stdout, stderr) {
-                    grunt.log.writeln("STDOUT: " + stdout);
-                    if (error) {
-                        if (verbose) {
-                            grunt.log.error("Error: " + error);
-                            grunt.log.error("STDERR: " + stderr);
-                        }
-                        callback(true, "Git tag error");
-                    }
-                });
-                grunt.log.ok();
-                callback(null, msg);
+            if (success) {
+                grunt.log.ok(msg || cmd);
+            } else {
+                // fail and stop execution of further tasks
+                grunt.fail.fatal("Failed when executing '" + cmd  + "'");
             }
-        ],
-            function rollback(err, results) {
-                if (err) {
-                    grunt.log.error("Error: " + err);
+        }
+
+        function checkout() {
+            var symbolicRef = shell.exec("git symbolic-ref HEAD", shellOpts).output;
+            if (symbolicRef) {
+                var splitted = symbolicRef.split('/');
+                // symbolic ref must contain 3 steps
+                // e.g ref/heads/master
+                if (splitted.length !== 3) {
+                    grunt.fail.fatal("Could not get the actual branch from symbolic ref");
                 }
-                var backBranch = currBranch !== undefined ? currBranch : "master";
-                if (verbose) {
-                    grunt.log.ok("Original Branch: " + backBranch);
-                    grunt.log.ok(results);
-                }
-                run("git reset --hard", "Reseting staged changes",
-                    function(error, stdout, stderr) {
-                        if (error) {
-                            grunt.log.error(error);
-                            grunt.log.error(stderr);
-                        }
-                        if (verbose) {
-                            grunt.log.ok(stdout);
-                        }
-                    }
-                );
-                run("git checkout " + backBranch, "Back to initial branch");
+                // the branch name is the last item of the array
+                currBranch = splitted[splitted.length - 1];
             }
-        );
+            run("git checkout --detach", "Detaching from current tree");
+        }
+
+        function add() {
+            grunt.log.oklns("ACTUAL VERSION ==> " + config.version);
+            grunt.log.oklns("BUILD FILES");
+            var filenames = [
+                grunt.config.get("path.main"),
+                grunt.config.get("path.min")
+            ];
+            // check the build files from the actual version
+            // and add the js files to be commited
+            var stringFiles = "";
+            for (var i = 0; i < filenames.length; i++) {
+                if (!grunt.file.exists(filenames[i])) {
+                    rollback();
+                    grunt.fail.fatal("Missing file " + filenames[i] + " is required");
+                }
+                stringFiles += filenames[i] + " ";
+            }
+            run("git add -f " + stringFiles, "Adding build files");
+        }
+
+        function commit() {
+            run("git commit -am 'Release " + version + " '", "Commiting release");
+        }
+
+        function tag() {
+            run("git tag " + version, "Tagging new version");
+        }
+
+        function push() {
+            run("git push origin " + version, "Pushing to new version branch");
+        }
+
+        function rollback() {
+            var backBranch = currBranch !== undefined ? currBranch : "master";
+            if (verbose) {
+                grunt.log.oklns("Original Branch: " + backBranch);
+            }
+            run("git reset --hard");
+            run("git checkout " + backBranch, "Resetting staged changes and back to initial branch");
+        }
+
+        try {
+            checkout();
+            add();
+            commit();
+            tag();
+            push();
+        } catch (err) {
+            grunt.fail.warn(err || "release failed");
+        } finally {
+            rollback();
+        }
     });
 };
