5f16a3a3452a86cd19f84cd545ad25daf4070c28
obiot <olivier.biot@me.com>
2014-10-10 15:12:59 +0800

    [#52] added a basic isometric example to test the new shape based Tile collision system
    based on the following one, courtesy of Pierre, that I had hanging
    around since a while :
    https://github.com/mfpierre/melonJS/tree/isometric_example

---
+++ examples/isometric_rpg/js/entities/entities.js
@@ -0,0 +1,58 @@
+
+/************************************************************************************/
+/*                                                                                  */
+/*        a player entity                                                           */
+/*                                                                                  */
+/************************************************************************************/
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings) {
+        // call the constructor
+        this._super(me.Entity, 'init', [x, y , settings]);
+
+        // disable gravity
+        this.body.gravity = 0;
+
+        // walking & jumping speed
+        this.body.setVelocity(2.5, 2.5);
+        this.body.setFriction(0.4,0.4);
+
+        // set the display around our position
+        me.game.viewport.follow(this, me.game.viewport.AXIS.BOTH);
+
+        // enable keyboard
+        me.input.bindKey(me.input.KEY.LEFT,  "left");
+        me.input.bindKey(me.input.KEY.RIGHT, "right");
+        me.input.bindKey(me.input.KEY.UP, "up");
+        me.input.bindKey(me.input.KEY.DOWN, "down");
+    },
+
+    /* -----
+
+        update the player pos
+
+    ------            */
+    update : function (dt) {
+
+        if (me.input.isKeyPressed('left')) {
+            // update the entity velocity
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        } else if (me.input.isKeyPressed('right')) {
+            // update the entity velocity
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        } else if (me.input.isKeyPressed('up')) {
+            // update the entity velocity
+            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+        } else if (me.input.isKeyPressed('down')) {
+            // update the entity velocity
+            this.body.vel.y += this.body.accel.y * me.timer.tick;
+        } else {
+            this.body.vel.x = 0;
+            this.body.vel.y = 0;
+        }
+
+        // check for collision with environment
+        this.body.update(dt);
+        
+        return true;
+    }
+});
---
+++ examples/isometric_rpg/js/game.js
@@ -0,0 +1,54 @@
+/**
+ * main
+ */
+var game = {
+
+    /**
+     *
+     * Initialize the application
+     */
+    onload: function() {
+
+        // init the video
+        if (!me.video.init('screen', me.video.CANVAS, 800, 600, true, 'auto')) {
+            alert("Sorry but your browser does not support html 5 canvas. Please try with another one!");
+            return;
+        }
+
+        // add "#debug" to the URL to enable the debug Panel
+        if (document.location.hash === "#debug") {
+            window.onReady(function () {
+                me.plugin.register.defer(this, me.debug.Panel, "debug", me.input.KEY.V);
+            });
+        }
+        
+        // set all ressources to be loaded
+        me.loader.onload = this.loaded.bind(this);
+
+        // set all ressources to be loaded
+        me.loader.preload(game.resources);
+
+        // load everything & display a loading screen
+        me.state.change(me.state.LOADING);
+    },
+
+
+    /**
+     * callback when everything is loaded
+     */
+    loaded: function ()    {
+
+        // set the "Play/Ingame" Screen Object
+        me.state.set(me.state.PLAY, new game.PlayScreen());
+
+        // set the fade transition effect
+        me.state.transition("fade","#FFFFFF", 250);
+
+        // register our objects entity in the object pool
+        me.pool.register("mainPlayer", game.PlayerEntity);
+
+        // switch to PLAY state
+        me.state.change(me.state.PLAY);
+    }
+};
+
