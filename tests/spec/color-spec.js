b12118cecc7f8a8ab55ece33ce81ce7ac3791cf3
Jay Oster <jay@kodewerx.org>
2014-10-19 01:38:42 -0700

    Expand me.Color tests

--- tests/spec/color-spec.js
+++ tests/spec/color-spec.js
@@ -11,16 +11,20 @@ describe("me.Color", function () {
             expect(red_color).toBeInstanceOf(me.Color);
         });
 
-        it("red_color.r = 255", function () {
+        it("red_color.r == 255", function () {
             expect(red_color.r).toEqual(255);
         });
 
-        it("red_color.g = 0", function () {
+        it("red_color.g == 0", function () {
             expect(red_color.g).toEqual(0);
         });
 
-        it("red_color.b = 0", function () {
+        it("red_color.b == 0", function () {
             expect(red_color.b).toEqual(0);
+        });
+
+        it("red_color.alpha == 1", function () {
+            expect(red_color.alpha).toEqual(1);
         });
 
         it("red_color hex value is #FF0000", function () {
@@ -30,22 +34,26 @@ describe("me.Color", function () {
     });
 
     describe("green_color", function () {
-        it("green_color.r = 0", function () {
+        it("green_color.r == 0", function () {
             expect(green_color.r).toEqual(0);
         });
 
-        it("green_color.g = 255", function () {
+        it("green_color.g == 128", function () {
             expect(green_color.g).toEqual(128);
         });
 
-        it("green_color.b = 0", function () {
+        it("green_color.b == 0", function () {
             expect(green_color.b).toEqual(0);
+        });
+
+        it("green_color.alpha == 1", function () {
+            expect(green_color.alpha).toEqual(1);
         });
 
         it("green_color hex value is #008000", function () {
             expect(green_color.toHex()).toEqual("#008000");
         });
-        
+
         it("(green_color + red_color) hex value is #FF8000", function () {
             expect(red_color.add(green_color).toHex()).toEqual("#FF8000");
         });
