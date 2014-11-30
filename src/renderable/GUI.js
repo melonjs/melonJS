a7e07a30877d32db4510d8658c06d14e970f85c3
Jorin Vermeulen <jorin.vermeulen@gmail.com>
2014-11-30 04:58:19 +0100

    Update GUI.js

--- src/renderable/GUI.js
+++ src/renderable/GUI.js
@@ -87,8 +87,8 @@
             // call the parent constructor
             this._super(me.Sprite, "init", [x, y,
                 ((typeof settings.image === "string") ? me.loader.getImage(settings.image) : settings.image),
-                settings.spritewidth,
-                settings.spriteheight]);
+                settings.framewidth,
+                settings.frameheight]);
 
             // GUI items use screen coordinates
             this.floating = true;
