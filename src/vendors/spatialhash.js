0b96ecf1a0734a0c102e8e34671f65bd0818cedd
Olivier Biot <olivier.biot@me.com>
2015-01-15 12:38:01 +0800

    [#624] cleaned spatialhash.js so that it passes linting

--- src/vendors/spatialhash.js
+++ src/vendors/spatialhash.js
@@ -32,93 +32,93 @@ only argument to the constructor.
 
 */
 
-(function(w) {
-  'use strict';
+(function () {
+   
+    var DEFAULT_POWER_OF_TWO = 5;
   
-  var DEFAULT_POWER_OF_TWO = 5;
+    function makeKeysFn(shift) {
+        return function (obj) {
+            var sx = obj.x >> shift,
+                sy = obj.y >> shift,
+                ex = (obj.x + obj.width) >> shift,
+                ey = (obj.y + obj.height) >> shift,
+                x, y, keys = [];
+            for (y = sy;y <= ey;y++) {
+                for (x = sx;x <= ex;x++) {
+                    keys.push("" + x + ":" + y);
+                }
+            }
+            return keys;
+        };
+    }
   
-  function makeKeysFn(shift) {
-    return function(obj) {
-      var sx = obj.x >> shift,
-        sy = obj.y >> shift,
-        ex = (obj.x + obj.width) >> shift,
-        ey = (obj.y + obj.height) >> shift,
-        x, y, keys = [];
-      for(y=sy;y<=ey;y++) {
-        for(x=sx;x<=ex;x++) {
-          keys.push("" + x + ":" + y);
+    /**
+    * @param {number} power_of_two - how many times the rects should be shifted
+    *                                when hashing
+    */
+    function SpatialHash(power_of_two) {
+        if (!power_of_two) {
+            power_of_two = DEFAULT_POWER_OF_TWO;
         }
-      }
-      return keys;
+        this.getKeys = makeKeysFn(power_of_two);
+        this.hash = {};
+        this.list = [];
+        this._lastTotalCleared = 0;
+    }
+  
+    SpatialHash.prototype.clear = function () {
+        var key;
+        for (key in this.hash) {
+            if (this.hash[key].length === 0) {
+                delete this.hash[key];
+            } else {
+                this.hash[key].length = 0;
+            }
+        }
+        this.list.length = 0;
     };
-  }  
   
-  /**
-  * @param {number} power_of_two - how many times the rects should be shifted
-  *                                when hashing
-  */
-  function SpatialHash(power_of_two) {
-    if (!power_of_two) {
-      power_of_two = DEFAULT_POWER_OF_TWO;
-    }
-    this.getKeys = makeKeysFn(power_of_two);
-    this.hash = {};
-    this.list = [];
-    this._lastTotalCleared = 0;
-  }
-  
-  SpatialHash.prototype.clear = function() {
-    var key;
-    for (key in this.hash) {
-      if (this.hash[key].length === 0) {
-        delete this.hash[key];
-      } else {
-        this.hash[key].length = 0;
-      }
-    }
-    this.list.length = 0;
-  };
-  
-  SpatialHash.prototype.getNumBuckets = function() {
-    var key, count = 0;
-    for (key in this.hash) {
-      if (this.hash.hasOwnProperty(key)) {
-        if (this.hash[key].length > 0) {
-          count++;
+    SpatialHash.prototype.getNumBuckets = function () {
+        var key, count = 0;
+        for (key in this.hash) {
+            if (this.hash.hasOwnProperty(key)) {
+                if (this.hash[key].length > 0) {
+                    count++;
+                }
+            }
         }
-      }
-    }
-    return count;
-    
-  };
+        return count;
+    };
   
-  SpatialHash.prototype.insert = function(obj, rect) {
-    var keys = this.getKeys(rect || obj), key, i;
-    this.list.push(obj);
-    for (i=0;i<keys.length;i++) {
-      key = keys[i];
-      if (this.hash[key]) {
-        this.hash[key].push(obj);
-      } else {
-        this.hash[key] = [obj];
-      }
-    }
-  };
+    SpatialHash.prototype.insert = function (obj, rect) {
+        var keys = this.getKeys(rect || obj), key, i;
+        this.list.push(obj);
+        for (i = 0;i < keys.length;i++) {
+            key = keys[i];
+            if (this.hash[key]) {
+                this.hash[key].push(obj);
+            } else {
+                this.hash[key] = [obj];
+            }
+        }
+    };
+
+    SpatialHash.prototype.retrieve = function (obj, rect) {
+        var ret = [], keys, i, key;
+        if (!obj && !rect) {
+            return this.list;
+        }
+        keys = this.getKeys(rect || obj);
+        for (i = 0;i < keys.length;i++) {
+            key = keys[i];
+            if (this.hash[key]) {
+                ret = ret.concat(this.hash[key]);
+            }
+        }
+        return ret;
+    };
   
-  SpatialHash.prototype.retrieve = function(obj, rect) {
-    var ret = [], keys, i, key;
-    if (!obj && !rect) {
-      return this.list;
-    }
-    keys = this.getKeys(rect || obj);
-    for (i=0;i<keys.length;i++) {
-      key = keys[i];
-      if (this.hash[key]) {
-        ret = ret.concat(this.hash[key]);
-      }
-    }
-    return ret;
-  };
-  
-  w.SpatialHash = SpatialHash;
-})(this);
+    //make SpatialHash available in the me namespace
+    me.SpatialHash = SpatialHash;
+
+})();
