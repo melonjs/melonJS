578e59e5a851dd98893a15ff5b57a4e784a373d8
Olivier Biot <olivier.biot@me.com>
2015-01-19 10:21:48 +0800

    clean the .glsl files on `grunt clean` but not the glsl subdir.

--- Gruntfile.js
+++ Gruntfile.js
@@ -161,6 +161,7 @@ module.exports = function (grunt) {
             ],
             jsdoc : [
                 "build/docs",
+                "build/glsl/*.glsl",
                 "./docs/**/*.*",
                 "./docs/scripts",
                 "./docs/styles",
