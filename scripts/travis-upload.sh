b23547a635d0de2782725b2a43fdc4ad2b055dda
Jay Oster <jay@kodewerx.org>
2015-06-27 00:47:54 -0700

    :construction_worker: Use a different method of conditional after_success
    - Matrix didn't seem to work at all. This time I'm using a shell script

---
+++ scripts/travis-upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ev
+
+if [ "${TRAVIS_NODE_VERSION}" = "0.10" ] ; then
+    travis-artifacts upload \
+        --path build/melonJS-${MELONJS_VERSION}.js \
+        --target-path artifacts/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}
+fi
