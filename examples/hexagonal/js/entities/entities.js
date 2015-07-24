3c1b72a31efd58255961708382d253586064ab42
Jay Oster <jay@kodewerx.org>
2015-07-24 00:28:47 -0700

    [#669] Remove more references to me.game.currentLevel

--- examples/hexagonal/js/entities/entities.js
+++ examples/hexagonal/js/entities/entities.js
@@ -5,30 +5,30 @@
 /*                                                                                  */
 /************************************************************************************/
 game.TextEntity = me.Renderable.extend({
-    init: function(x, y, width, height) {
+    init: function (x, y, width, height) {
         var self = this;
         this.text = "?,?";
         this.font_size = 20;
         this.font = new me.Font("courier", this.font_size, "white");
-        
+
         // call the constructor
         this._super(me.Renderable, 'init', [x, y , width, height]);
         this.floating = true;
-        
+
         this.pointerDown = me.event.subscribe("pointermove", function (event) {
             self.text = "?,?";
             //self.text = Math.round(event.gameX) + "," + Math.round(event.gameY);
-            if(me.game.currentLevel){
-                var layer = me.game.currentLevel.getLayers()[0];
+            if (me.levelDirector.getCurrentLevelId()) {
+                var layer = me.game.world.getChildByName("Ground")[0];
                 var tile = layer.getTile(event.gameWorldX, event.gameWorldY);
-                if(tile){
+                if (tile) {
                     self.text = tile.col + "," + tile.row;
                 }
             }
         });
     },
 
-    draw : function(renderer){
+    draw : function (renderer) {
         renderer.setColor("black");
         renderer.fillRect(
             this.left,  this.top,
@@ -36,8 +36,8 @@ game.TextEntity = me.Renderable.extend({
         );
         this.font.draw(renderer,this.text,this.pos.x, this.pos.y);
     },
-    
-    update : function(){
+
+    update : function () {
         return true;
     },
 });
