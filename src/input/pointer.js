melonjs/melonJS

commit 2d5483ab712d82270d8e8967514f43bb9e0d8755

[#643] further improvements (see description)
- removed duplicated function in me.Rect
- add a call to `recalc` when directly manipulating the points array in
the me.Rect setter
- removed the use of `shapeType` when adding a shape to a body , or
checking for mouse over event. Only place using it is now for collision
detection between ellipse and polygons

src/input/pointer.js
@@ -295,8 +295,12 @@
                 }
                 
                 var region = handlers.region;
-                var eventInBounds = region.getBounds().containsPoint(e.gameX, e.gameY) &&
-                                    (region.shapeType === "Rectangle" || region.containsPoint(e.gameX, e.gameY));
+                var bounds = region.getBounds();
+                var eventInBounds =
+                    // check the shape bounding box first
+                    bounds.containsPoint(e.gameX, e.gameY) &&
+                    // then check more precisely if needed
+                    (bounds !== region || region.containsPoint(e.gameX, e.gameY));
 
                 switch (activeEventList.indexOf(e.type)) {
                     case POINTER_MOVE:

src/physics/body.js
@@ -183,13 +183,7 @@
          * @return {Number} the shape array length
          */
         addShape : function (shape, batchInsert) {
-            if (shape.shapeType === "Rectangle") {
-                // ensure that rect shape are managed as polygon
-                this.shapes.push(shape.toPolygon());
-            } else {
-                // else polygon or circle
-                this.shapes.push(shape);
-            }
+            this.shapes.push(shape);
 
             if (batchInsert !== true) {
                 // update the body bounds to take in account the added shape

src/shapes/rectangle.js
@@ -29,10 +29,8 @@
                 new me.Vector2d(), new me.Vector2d(),
                 new me.Vector2d(), new me.Vector2d()
             ];
-
-            // the shape type
-            this.shapeType = "Rectangle";
-
+            
+            this.shapeType = "Polygon";
             this.setShape(x, y, w, h);
         },
 
@@ -213,18 +211,6 @@
 
         /**
          * check if this rectangle contains the specified point
-         * @name containsPointV
-         * @memberOf me.Rect
-         * @function
-         * @param  {me.Vector2d} point
-         * @return {boolean} true if contains
-         */
-        containsPointV: function (v) {
-            return this.containsPoint(v.x, v.y);
-        },
-
-        /**
-         * check if this rectangle contains the specified point
          * @name containsPoint
          * @memberOf me.Rect
          * @function
@@ -327,6 +313,7 @@
         set : function (value) {
             this._width = value;
             this.points[1].x = this.points[2].x = value;
+            this.recalc();
         },
         configurable : true
     });
@@ -345,6 +332,7 @@
         set : function (value) {
             this._height = value;
             this.points[2].y = this.points[3].y = value;
+            this.recalc();
         },
         configurable : true
     });

commit e344c2ea9c694f975ec91301d859341d9319846a

[#643] That's not right!
- This bug was introduced in 2d5483ab712d82270d8e8967514f43bb9e0d8755
- JavaScript logical conditions work like this:
    - for && : evaluate and return left side if false ; else evaluate and return right side
    - for || : evaluate and return left side if true ; else evaluate and return right side
    - see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_Operators
- Therefore, this equality condition is attempting to optimize a special case where the `containsPoint` method may be called twice
- With this bug the optimization fails, and the method is called twice! Even worse, `region.containsPoint` is never called at all!

src/input/pointer.js
@@ -300,7 +300,7 @@
                     // check the shape bounding box first
                     bounds.containsPoint(e.gameX, e.gameY) &&
                     // then check more precisely if needed
-                    (bounds !== region || region.containsPoint(e.gameX, e.gameY));
+                    (bounds === region || region.containsPoint(e.gameX, e.gameY));
 
                 switch (activeEventList.indexOf(e.type)) {
                     case POINTER_MOVE: