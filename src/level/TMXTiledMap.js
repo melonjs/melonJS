melonjs/melonJS

commit 6cec279c4c1a1ecab093edb683d33a7bcb36a40c

Simplify me.ImageLayer instantiation
- Fixes a bug with the XML parser being unable to normalize the ratio and anchorPoint properties
- Pass all properties to the me.ImageLayer constructor directly, instead of augmenting the object after it is created
- This fixes an issue with defining `anchorPoint` as a JSON value in a TMX property. Thanks to @steelwil for the report

src/level/TMXTiledMap.js
@@ -99,33 +99,20 @@
      * @ignore
      */
     function readImageLayer(map, data, z) {
-        // extract layer information
-        var ilx = +data.x || 0;
-        var ily = +data.y || 0;
-        var iln = data.name;
-        var ilsrc = data.image;
-
         // create the layer
         var imageLayer = new me.ImageLayer(
-            ilx, ily, {
-                name: iln,
-                image: ilsrc,
-                z : z
-            }
+            +data.x || 0,
+            +data.y || 0,
+            Object.assign({
+                name: data.name,
+                image: data.image,
+                z: z
+            }, data.properties)
         );
 
         // set some additional flags
         var visible = typeof(data.visible) !== "undefined" ? data.visible : true;
         imageLayer.setOpacity(visible ? +data.opacity : 0);
-
-        // check if we have any additional properties
-        me.TMXUtils.applyTMXProperties(imageLayer, data);
-
-        // make sure ratio is a vector (backward compatibility)
-        if (typeof(imageLayer.ratio) === "number") {
-            var ratio = imageLayer.ratio;
-            imageLayer.ratio = new me.Vector2d(ratio, ratio);
-        }
 
         return imageLayer;
     }

src/level/TMXUtils.js
@@ -27,6 +27,7 @@
          * @ignore
          */
         function setTMXValue(name, value) {
+            value = "" + value;
             if (!value || value.isBoolean()) {
                 // if value not defined or boolean
                 value = value ? (value === "true") : true;
@@ -68,10 +69,10 @@
                     var attribute = elt.attributes.item(j);
                     if (typeof(attribute.name) !== "undefined") {
                         // DOM4 (Attr no longer inherit from Node)
-                        obj[attribute.name] = setTMXValue(attribute.name, attribute.value);
+                        obj[attribute.name] = attribute.value;
                     } else {
                         // else use the deprecated ones
-                        obj[attribute.nodeName] = setTMXValue(attribute.nodeName, attribute.nodeValue);
+                        obj[attribute.nodeName] = attribute.nodeValue;
                     }
                 }
             }
@@ -174,7 +175,7 @@
 
                 case "property":
                     var property = api.parse(item);
-                    obj[property.name] = "" + property.value;
+                    obj[property.name] = setTMXValue(property.name, property.value);
                     break;
 
                 default:

src/level/TMXLayer.js
@@ -150,14 +150,6 @@
                 }
             }
 
-            // default value for repeat
-            this._repeat = "repeat";
-
-            this.repeatX = true;
-            this.repeatY = true;
-
-            this.createPattern();
-
             /**
              * Define if and how an Image Layer should be repeated.<br>
              * By default, an Image Layer is repeated both vertically and horizontally.<br>
@@ -198,6 +190,8 @@
                     this.createPattern();
                 }
             });
+
+            this.repeat = settings.repeat || "repeat";
         },
 
         // called when the layer is added to the game world or a container

commit 5b13909aba6bd04e643a7c2063cc293807010f31

Fix ImageLayer property normalization with JSON format maps
- Broken by 6cec279c4c1a1ecab093edb683d33a7bcb36a40c

src/level/TMXTiledMap.js
@@ -99,6 +99,9 @@
      * @ignore
      */
     function readImageLayer(map, data, z) {
+        // Normalize properties
+        me.TMXUtils.applyTMXProperties(data.properties, data);
+
         // create the layer
         var imageLayer = new me.ImageLayer(
             +data.x || 0,

src/level/TMXUtils.js
@@ -27,7 +27,11 @@
          * @ignore
          */
         function setTMXValue(name, value) {
-            value = "" + value;
+            if (typeof(value) !== "string") {
+                // Value is already normalized
+                return value;
+            }
+
             if (!value || value.isBoolean()) {
                 // if value not defined or boolean
                 value = value ? (value === "true") : true;