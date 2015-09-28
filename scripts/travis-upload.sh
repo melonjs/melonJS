6d99a34b73b104f41fe2ff04539975b44ab14886
Jay Oster <jay@kodewerx.org>
2015-09-27 17:01:06 -0700

    Replace deprecated travis-artifacts gem with the new kid in town
    - That's nice! We can now put the AWS credentials into env variables in the repo settings, instead of encrypted in .travis.yml
    - I hope this works!

--- scripts/travis-upload.sh
+++ scripts/travis-upload.sh
@@ -2,7 +2,10 @@
 set -ev
 
 if [ "${TRAVIS_NODE_VERSION}" = "0.10" ] ; then
-    travis-artifacts upload \
-        --path build/melonJS.js \
-        --target-path artifacts/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}
+    artifacts upload \
+        --bucket melonjs-builds \
+        --s3-region us-east-1 \
+        --permissions public-read \
+        --target-paths artifacts/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER} \
+        build/melonJS.js
 fi
