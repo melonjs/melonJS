72cda340bd5e9411279d489d317cb1009627b567
Olivier Biot <olivier.biot@me.com>
2016-03-11 11:18:18 +0800

    fix documentation for `me.input.KEY`, similarly to `me.input.GAMEPAD`

--- src/input/keyboard.js
+++ src/input/keyboard.js
@@ -116,113 +116,208 @@
      */
 
     /**
-     * Almost all keyboard keys that have ASCII code, like:
-     * LEFT, UP, RIGHT, DOWN, ENTER, SHIFT, CTRL, ALT, ESC, SPACE, TAB, BACKSPACE, PAUSE,
-     * PAGE_UP, PAGE_DOWN, INSERT, DELETE, CAPS_LOCK, NUM_LOCK, SCROLL_LOCK, PRINT_SCREEN,
-     * Keys [0..9], [A..Z], [NUMPAD0..NUMPAD9], [F1..F12]
+     * standard keyboard constants
      * @public
      * @enum {number}
-     * @name KEY
+     * @namespace KEY
      * @memberOf me.input
      */
     api.KEY = {
+        /** @memberOf me.input.KEY */
         "BACKSPACE" : 8,
+        /** @memberOf me.input.KEY */
         "TAB" : 9,
+        /** @memberOf me.input.KEY */
         "ENTER" : 13,
+        /** @memberOf me.input.KEY */
         "SHIFT" : 16,
+        /** @memberOf me.input.KEY */
         "CTRL" : 17,
+        /** @memberOf me.input.KEY */
         "ALT" : 18,
+        /** @memberOf me.input.KEY */
         "PAUSE" : 19,
+        /** @memberOf me.input.KEY */
         "CAPS_LOCK" : 20,
+        /** @memberOf me.input.KEY */
         "ESC" : 27,
+        /** @memberOf me.input.KEY */
         "SPACE" : 32,
+        /** @memberOf me.input.KEY */
         "PAGE_UP" : 33,
+        /** @memberOf me.input.KEY */
         "PAGE_DOWN" : 34,
+        /** @memberOf me.input.KEY */
         "END" : 35,
+        /** @memberOf me.input.KEY */
         "HOME" : 36,
+        /** @memberOf me.input.KEY */
         "LEFT" : 37,
+        /** @memberOf me.input.KEY */
         "UP" : 38,
+        /** @memberOf me.input.KEY */
         "RIGHT" : 39,
+        /** @memberOf me.input.KEY */
         "DOWN" : 40,
+        /** @memberOf me.input.KEY */
         "PRINT_SCREEN" : 42,
+        /** @memberOf me.input.KEY */
         "INSERT" : 45,
+        /** @memberOf me.input.KEY */
         "DELETE" : 46,
+        /** @memberOf me.input.KEY */
         "NUM0" : 48,
+        /** @memberOf me.input.KEY */
         "NUM1" : 49,
+        /** @memberOf me.input.KEY */
         "NUM2" : 50,
+        /** @memberOf me.input.KEY */
         "NUM3" : 51,
+        /** @memberOf me.input.KEY */
         "NUM4" : 52,
+        /** @memberOf me.input.KEY */
         "NUM5" : 53,
+        /** @memberOf me.input.KEY */
         "NUM6" : 54,
+        /** @memberOf me.input.KEY */
         "NUM7" : 55,
+        /** @memberOf me.input.KEY */
         "NUM8" : 56,
+        /** @memberOf me.input.KEY */
         "NUM9" : 57,
+        /** @memberOf me.input.KEY */
         "A" : 65,
+        /** @memberOf me.input.KEY */
         "B" : 66,
+        /** @memberOf me.input.KEY */
         "C" : 67,
+        /** @memberOf me.input.KEY */
         "D" : 68,
+        /** @memberOf me.input.KEY */
         "E" : 69,
+        /** @memberOf me.input.KEY */
         "F" : 70,
+        /** @memberOf me.input.KEY */
         "G" : 71,
+        /** @memberOf me.input.KEY */
         "H" : 72,
+        /** @memberOf me.input.KEY */
         "I" : 73,
+        /** @memberOf me.input.KEY */
         "J" : 74,
+        /** @memberOf me.input.KEY */
         "K" : 75,
+        /** @memberOf me.input.KEY */
         "L" : 76,
+        /** @memberOf me.input.KEY */
         "M" : 77,
+        /** @memberOf me.input.KEY */
         "N" : 78,
+        /** @memberOf me.input.KEY */
         "O" : 79,
+        /** @memberOf me.input.KEY */
         "P" : 80,
+        /** @memberOf me.input.KEY */
         "Q" : 81,
+        /** @memberOf me.input.KEY */
         "R" : 82,
+        /** @memberOf me.input.KEY */
         "S" : 83,
+        /** @memberOf me.input.KEY */
         "T" : 84,
+        /** @memberOf me.input.KEY */
         "U" : 85,
+        /** @memberOf me.input.KEY */
         "V" : 86,
+        /** @memberOf me.input.KEY */
         "W" : 87,
+        /** @memberOf me.input.KEY */
         "X" : 88,
+        /** @memberOf me.input.KEY */
         "Y" : 89,
+        /** @memberOf me.input.KEY */
         "Z" : 90,
+        /** @memberOf me.input.KEY */
         "WINDOW_KEY" : 91,
+        /** @memberOf me.input.KEY */
         "NUMPAD0" : 96,
+        /** @memberOf me.input.KEY */
         "NUMPAD1" : 97,
+        /** @memberOf me.input.KEY */
         "NUMPAD2" : 98,
+        /** @memberOf me.input.KEY */
         "NUMPAD3" : 99,
+        /** @memberOf me.input.KEY */
         "NUMPAD4" : 100,
+        /** @memberOf me.input.KEY */
         "NUMPAD5" : 101,
+        /** @memberOf me.input.KEY */
         "NUMPAD6" : 102,
+        /** @memberOf me.input.KEY */
         "NUMPAD7" : 103,
+        /** @memberOf me.input.KEY */
         "NUMPAD8" : 104,
+        /** @memberOf me.input.KEY */
         "NUMPAD9" : 105,
+        /** @memberOf me.input.KEY */
         "MULTIPLY" : 106,
+        /** @memberOf me.input.KEY */
         "ADD" : 107,
+        /** @memberOf me.input.KEY */
         "SUBSTRACT" : 109,
+        /** @memberOf me.input.KEY */
         "DECIMAL" : 110,
+        /** @memberOf me.input.KEY */
         "DIVIDE" : 111,
+        /** @memberOf me.input.KEY */
         "F1" : 112,
+        /** @memberOf me.input.KEY */
         "F2" : 113,
+        /** @memberOf me.input.KEY */
         "F3" : 114,
+        /** @memberOf me.input.KEY */
         "F4" : 115,
+        /** @memberOf me.input.KEY */
         "F5" : 116,
+        /** @memberOf me.input.KEY */
         "F6" : 117,
+        /** @memberOf me.input.KEY */
         "F7" : 118,
+        /** @memberOf me.input.KEY */
         "F8" : 119,
+        /** @memberOf me.input.KEY */
         "F9" : 120,
+        /** @memberOf me.input.KEY */
         "F10" : 121,
+        /** @memberOf me.input.KEY */
         "F11" : 122,
+        /** @memberOf me.input.KEY */
         "F12" : 123,
+        /** @memberOf me.input.KEY */
         "NUM_LOCK" : 144,
+        /** @memberOf me.input.KEY */
         "SCROLL_LOCK" : 145,
+        /** @memberOf me.input.KEY */
         "SEMICOLON" : 186,
+        /** @memberOf me.input.KEY */
         "PLUS" : 187,
+        /** @memberOf me.input.KEY */
         "COMMA" : 188,
+        /** @memberOf me.input.KEY */
         "MINUS" : 189,
+        /** @memberOf me.input.KEY */
         "PERIOD" : 190,
+        /** @memberOf me.input.KEY */
         "FORWAND_SLASH" : 191,
+        /** @memberOf me.input.KEY */
         "GRAVE_ACCENT" : 192,
+        /** @memberOf me.input.KEY */
         "OPEN_BRACKET" : 219,
+        /** @memberOf me.input.KEY */
         "BACK_SLASH" : 220,
+        /** @memberOf me.input.KEY */
         "CLOSE_BRACKET" : 221,
+        /** @memberOf me.input.KEY */
         "SINGLE_QUOTE" : 222
     };
 
