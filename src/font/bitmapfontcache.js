d8d72faefda2f4f0adef1adb0924e2f37c2ec95b
Aaron McLeod <aaron.g.mcleod@gmail.com>
2016-05-17 22:02:47 -0400

    Working on cache build. Might need to store array of regions instead.

--- src/font/bitmapfontcache.js
+++ src/font/bitmapfontcache.js
@@ -16,8 +16,86 @@
      * @constructor
      */
     me.BitmapFontCache = me.Renderable.extend({
-        init: function () {
+        init: function (bitmapFont, useRoundedPositioning) {
+            this.bitmapFont = bitmapFont;
+            this.useRoundedPositioning = useRoundedPositioning;
+            this.vertexDataCount = 0;
+            this.vertices = [];
+            this.layouts = [];
+        },
 
+        _requireGlyphs: function (layout) {
+            var newGlyphCount = 0;
+            for (var i = 0; i < layout.runs.length; i++) {
+                newGlyphCount += layout.runs[i].glyphs.length;
+            }
+
+            var vertexCount = this.vertexDataCount + newGlyphCount * 20;
+            if (this.vertices.length < vertexCount) {
+                var newVertices = new Array(vertexCount);
+                for (var i = 0; i < this.vertexDataCount; i++) {
+                    newVertices[i] = this.vertices[i];
+                }
+                this.vertices = newVertices;
+            }
+        },
+
+        addToCache: function (layout, x, y) {
+            this.layouts.push(layout);
+            this._requireGlyphs(layout);
+            for (var i = 0; i < layout.runs.length; i++) {
+                var run = layout.runs[i];
+                var glyphs = run.glyphs;
+                var xAdvances = run.xAdvances;
+                var gx = x + run.x, gy = y + run.y;
+                for (var ii = 0; ii < glyphs.length; ii++) {
+                    var glyph = glyphs[ii];
+                    gx += xAdvances[ii];
+                    this.addGlyph(glyph, gx, gy);
+                }
+            }
+        },
+
+        addGlyph: function (glyph, x, y) {
+            var scaleX = this.font.data.scale.x;
+            var scaleY = this.font.data.scale.y;
+            x += glyph.xoffset * scaleX;
+            y += glyph.yoffset * scaleY;
+            var width = glyph.width * scaleX, height = glyph.height * scaleY;
+            var u = glyph.u, u2 = glyph.u2, v = glyph.v, v2 = glyph.v2;
+
+            if (integer) {
+                x = Math.round(x);
+                y = Math.round(y);
+                width = Math.round(width);
+                height = Math.round(height);
+            }
+            var x2 = x + width, y2 = y + height;
+
+            var page = glyph.page;
+            this.vertexDataCount += 20;
+
+            var vertices = this.vertices;
+            var idx = this.vertexDataCount;
+            vertices[idx++] = x;
+            vertices[idx++] = y;
+            vertices[idx++] = u;
+            vertices[idx++] = v;
+
+            vertices[idx++] = x;
+            vertices[idx++] = y2;
+            vertices[idx++] = u;
+            vertices[idx++] = v2;
+
+            vertices[idx++] = x2;
+            vertices[idx++] = y2;
+            vertices[idx++] = u2;
+            vertices[idx++] = v2;
+
+            vertices[idx++] = x2;
+            vertices[idx++] = y;
+            vertices[idx++] = u2;
+            vertices[idx] = v;
         }
     });
 })();
