f2ad18568cd4dad75265f0cd2b54344251e335c1
obiot <olivier.biot@me.com>
2016-10-18 10:23:09 +0700

    [#642] add tests for renderables absolute position

--- tests/spec/renderable-spec.js
+++ tests/spec/renderable-spec.js
@@ -25,4 +25,34 @@ describe("me.Renderable", function () {
             expect(renderable.getBounds().height).toEqual(20);
         });
     });
+
+    describe("getAbsoluteBounds returns the correct value", function () {
+        var rootContainer = new me.Container(0, 0, 1000, 1000);
+        var childContainer = new me.Container(100, 100, 500, 500);
+        var renderable = new me.Renderable(50, 50, 50, 50);
+
+        it("create and add a child container to the root container", function () {
+            rootContainer._root = true;
+            rootContainer.addChild(childContainer);
+            expect(childContainer.isAttachedToRoot()).toEqual(true);
+        });
+
+        it("renderable should have a correct absolute position once added", function () {
+            childContainer.addChild(renderable);
+            expect(renderable.getBounds().pos.x).toEqual(150);
+            expect(renderable.getBounds().pos.y).toEqual(150);
+        });
+
+        it("changing the renderable position, change the absolute pos", function () {
+            renderable.pos.set(200, 100, 0);
+            expect(renderable.getBounds().pos.x).toEqual(300);
+            expect(renderable.getBounds().pos.y).toEqual(200);
+        });
+
+        it("changing the parent container position, also change the renderable absolute pos", function () {
+            childContainer.pos.set(200, 200, 0);
+            expect(renderable.getBounds().pos.x).toEqual(400);
+            expect(renderable.getBounds().pos.y).toEqual(300);
+        });
+    });
 });
