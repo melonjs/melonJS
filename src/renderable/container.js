e18f0946d4e95af16634249de3736aefe509c832
obiot <olivier.biot@me.com>
2016-11-10 13:42:13 +0800

    [#830] add a `forEach` function to Container object (thanks krojew for the suggestion)

--- src/renderable/container.js
+++ src/renderable/container.js
@@ -200,6 +200,42 @@
         },
 
         /**
+         * The forEach() method executes a provided function once per child element. <br>
+         * callback is invoked with three arguments: <br>
+         *    - the element value <br>
+         *    - the element index <br>
+         *    - the array being traversed <br>
+         * @name forEach
+         * @memberOf me.Container
+         * @function
+         * @param {Function} callback
+         * @param {Object} [thisArg] value to use as this(i.e reference Object) when executing callback.
+         * @example
+         * // iterate through all children of the root container
+         * me.game.world.forEach(function (child) {
+         *    // do something with the child
+         * });
+         */
+        forEach : function (callback, thisArg) {
+            var context = this, i = 0;
+
+            var len = this.children.length;
+
+            if (typeof callback !== "function") {
+                throw new me.Container.Error(callback + " is not a function");
+            }
+
+            if (arguments.length > 1) {
+                context = thisArg;
+            }
+
+            while (i < len) {
+                callback.call(context, this.children[i], i, this.children);
+                i++;
+            }
+        },
+
+        /**
          * Swaps the position (z-index) of 2 children
          * @name swapChildren
          * @memberOf me.Container
