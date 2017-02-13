c1e1795424e77cb0e7454d216f638700e3dcfffd
obiot <olivier.biot@me.com>
2017-02-13 10:11:42 +0800

    small update to match with the MDN polyfill

--- src/lang/math.js
+++ src/lang/math.js
@@ -19,9 +19,14 @@ if (!Math.sign) {
      * @return {number} sign of the number
      */
     Math.sign = function(x) {
+        // If x is NaN, the result is NaN.
+        // If x is -0, the result is -0.
+        // If x is +0, the result is +0.
+        // If x is negative and not -0, the result is -1.
+        // If x is positive and not +0, the result is +1.
         x = +x; // convert to a number
         if (x === 0 || isNaN(x)) {
-            return x;
+            return Number(x);
         }
         return x > 0 ? 1 : -1;
     };
