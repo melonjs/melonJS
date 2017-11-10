8454aa7b60df327a96e48da9d4143415b9976519
Olivier Biot <olivier.biot@me.com>
2017-11-10 11:46:47 +0800

    slightly refactore me.ColorLayer to allow use of RGB cycling effects
    also move it to the Renderable folder, as this is not a TMX specific object/renderable

--- src/level/TMXLayer.js
+++ src/level/TMXLayer.js
@@ -27,58 +27,6 @@
     }
 
     /**
-     * a generic Color Layer Object
-     * @class
-     * @extends me.Renderable
-     * @memberOf me
-     * @constructor
-     * @param {String} name Layer name
-     * @param {me.Color|String} color CSS color
-     * @param {Number} z z-index position
-     */
-    me.ColorLayer = me.Renderable.extend({
-        /**
-         * Constructor
-         * @ignore
-         */
-        init: function (name, color, z) {
-            // parent constructor
-            this._super(me.Renderable, "init", [0, 0, Infinity, Infinity]);
-
-            // apply given parameters
-            this.name = name;
-            this.pos.z = z;
-            this.floating = true;
-
-            // parse the given color
-            if (color instanceof me.Color) {
-                this.color = color.toRGBA();
-            } else {
-                // string (#RGB, #ARGB, #RRGGBB, #AARRGGBB)
-                var _col = me.pool.pull("me.Color");
-                this.color = _col.parseCSS(color).toRGBA();
-                me.pool.push(_col);
-            }
-
-            this.anchorPoint.set(0, 0);
-        },
-
-        /**
-         * draw the color layer
-         * @ignore
-         */
-        draw : function (renderer, rect) {
-            var vpos = me.game.viewport.pos;
-            renderer.setColor(this.color);
-            renderer.fillRect(
-                rect.left - vpos.x, rect.top - vpos.y,
-                rect.width, rect.height
-            );
-            renderer.setColor("#fff");
-        }
-    });
-
-    /**
      * a generic Image Layer Object
      * @class
      * @extends me.Renderable
---
+++ src/renderable/colorlayer.js
@@ -0,0 +1,66 @@
+/*
+ * MelonJS Game Engine
+ * Copyright (C) 2011 - 2017, Olivier Biot, Jason Oster, Aaron McLeod
+ * http://www.melonjs.org
+ *
+ */
+(function () {
+
+    /**
+     * a generic Color Layer Object
+     * @class
+     * @extends me.Renderable
+     * @memberOf me
+     * @constructor
+     * @param {String} name Layer name
+     * @param {me.Color|String} color CSS color
+     * @param {Number} z z-index position
+     */
+    me.ColorLayer = me.Renderable.extend({
+        /**
+         * Constructor
+         * @ignore
+         */
+        init: function (name, color, z) {
+            // parent constructor
+            this._super(me.Renderable, "init", [0, 0, Infinity, Infinity]);
+
+            // apply given parameters
+            this.name = name;
+            this.pos.z = z;
+            this.floating = true;
+
+            /**
+             * the layer color component
+             * @public
+             * @type me.Color
+             * @name me.ColorLayer#color
+             */
+
+            // parse the given color
+            if (color instanceof me.Color) {
+                this.color = color;
+            } else {
+                // string (#RGB, #ARGB, #RRGGBB, #AARRGGBB)
+                this.color = me.pool.pull("me.Color").parseCSS(color);
+            }
+            this.anchorPoint.set(0, 0);
+        },
+
+        /**
+         * draw the color layer
+         * @ignore
+         */
+        draw : function (renderer, rect) {
+            var color = renderer.getColor();
+            var vpos = me.game.viewport.pos;
+            renderer.setColor(this.color);
+            renderer.fillRect(
+                rect.left - vpos.x, rect.top - vpos.y,
+                rect.width, rect.height
+            );
+            renderer.setColor(color);
+        }
+    });
+
+})();
