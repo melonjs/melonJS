2f8ce2ad900ac16b2debdd7e213c61b779417c70
Olivier Biot <olivier.biot@me.com>
2017-12-26 11:07:55 +0800

    [#833][#793] apply anchor point and transform to me.Sprite Bounding Rect
    allows to properly takes in account anchor and transform for any me.Sprite object (including me.GUI), including proper detection of pointer Events.
    
    this is first partial implementation only for me.Sprite objects, as applying the same to the base me.Renderable class will trigger further breaking changes in me.Entity
    
    TODO :
    - me.Renderable class, including me.Entity
    - properly fix the debugPanel (Sprite BB drawing is off for now)

--- plugins/debug/debugPanel.js
+++ plugins/debug/debugPanel.js
@@ -328,6 +328,8 @@
                         var ax = this.anchorPoint.x * bounds.width,
                             ay = this.anchorPoint.y * bounds.height;
 
+                        renderer.save();
+
                         // translate back as the bounds position
                         // is already adjusted to the anchor Point
                         renderer.translate(ax, ay);
@@ -335,17 +337,7 @@
                         renderer.setColor("green");
                         renderer.drawShape(bounds);
 
-                        renderer.translate(-ax, -ay);
-
-                        if (this.body) {
-                            renderer.translate(this.pos.x, this.pos.y);
-                            // draw all defined shapes
-                            renderer.setColor("red");
-                            for (var i = this.body.shapes.length, shape; i--, (shape = this.body.shapes[i]);) {
-                                renderer.drawShape(shape);
-                                _this.counters.inc("shapes");
-                            }
-                        }
+                        renderer.restore();
                     }
                 }
             });
