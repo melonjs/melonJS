a7c714c84847562c8d4bb9fbe9636c561be33fac
Olivier Biot <olivier.biot@me.com>
2018-01-08 11:15:49 +0800

    [#895] new "sticket-knight" example for Collection of Image
    Strip down version of the original one (from Tiled), with just the tile rendereding.
    
    UI sample definitely worth checking as well
    
    Know issues : issue with rotation is some cases (see the tower)

---
+++ examples/sticker-knight/js/resources.js
@@ -0,0 +1,93 @@
+game.resources = [
+
+    /* Graphics.
+     * @example
+     * { name: "example", type:"image", src: "data/img/example.png" },
+     */
+    { name: "alter",  type:"image", src: "data/map/alter.png"},
+    { name: "blobBlue",  type:"image", src: "data/map/blobBlue.png"},
+    { name: "blobGreen",  type:"image", src: "data/map/blobGreen.png"},
+    { name: "blue",  type:"image", src: "data/map/blue.png"},
+    { name: "bombStroked",  type:"image", src: "data/map/bombStroked.png"},
+    { name: "castleWall",  type:"image", src: "data/map/castleWall.png"},
+    { name: "cloud",  type:"image", src: "data/map/cloud.png"},
+    { name: "column1",  type:"image", src: "data/map/column1.png"},
+    { name: "column2",  type:"image", src: "data/map/column2.png"},
+    { name: "doorBlueStroked",  type:"image", src: "data/map/doorBlueStroked.png"},
+    { name: "doorGreenStroke",  type:"image", src: "data/map/doorGreenStroke.png"},
+    { name: "doorRedStroked",  type:"image", src: "data/map/doorRedStroked.png"},
+    { name: "doorStroked",  type:"image", src: "data/map/doorStroked.png"},
+    { name: "earthWall",  type:"image", src: "data/map/earthWall.png"},
+    { name: "exit",  type:"image", src: "data/map/exit.png"},
+    { name: "flare",  type:"image", src: "data/map/flare.png"},
+    { name: "gemBlueStroked",  type:"image", src: "data/map/gemBlueStroked.png"},
+    { name: "gemRedStroked",  type:"image", src: "data/map/gemRedStroked.png"},
+    { name: "grassLarge",  type:"image", src: "data/map/grassLarge.png"},
+    { name: "grassSmall",  type:"image", src: "data/map/grassSmall.png"},
+    { name: "grey",  type:"image", src: "data/map/grey.png"},
+    { name: "hero",  type:"image", src: "data/map/hero.png"},
+    { name: "keyGreenStroked",  type:"image", src: "data/map/keyGreenStroked.png"},
+    { name: "keyRedStroked",  type:"image", src: "data/map/keyRedStroked.png"},
+    { name: "keyYellowStroked",  type:"image", src: "data/map/keyYellowStroked.png"},
+    { name: "platform1",  type:"image", src: "data/map/platform1.png"},
+    { name: "platform2",  type:"image", src: "data/map/platform2.png"},
+    { name: "platform3",  type:"image", src: "data/map/platform3.png"},
+    { name: "platform4",  type:"image", src: "data/map/platform4.png"},
+    { name: "platformBase1",  type:"image", src: "data/map/platformBase1.png"},
+    { name: "platformBase2",  type:"image", src: "data/map/platformBase2.png"},
+    { name: "platformBase3",  type:"image", src: "data/map/platformBase3.png"},
+    { name: "platformBase4",  type:"image", src: "data/map/platformBase4.png"},
+    { name: "platformBlock1",  type:"image", src: "data/map/platformBlock1.png"},
+    { name: "platformBlock2",  type:"image", src: "data/map/platformBlock2.png"},
+    { name: "platformBlock3",  type:"image", src: "data/map/platformBlock3.png"},
+    { name: "platformBlock4",  type:"image", src: "data/map/platformBlock4.png"},
+    { name: "platformConnector1",  type:"image", src: "data/map/platformConnector1.png"},
+    { name: "platformConnector2",  type:"image", src: "data/map/platformConnector2.png"},
+    { name: "platformConnector3",  type:"image", src: "data/map/platformConnector3.png"},
+    { name: "platformConnector4",  type:"image", src: "data/map/platformConnector4.png"},
+    { name: "pushBlock1",  type:"image", src: "data/map/pushBlock1.png"},
+    { name: "pushBlock2",  type:"image", src: "data/map/pushBlock2.png"},
+    { name: "pushBlock3",  type:"image", src: "data/map/pushBlock3.png"},
+    { name: "shadow",  type:"image", src: "data/map/shadow.png"},
+    { name: "shieldStroked",  type:"image", src: "data/map/shieldStroked.png"},
+    { name: "sign",  type:"image", src: "data/map/sign.png"},
+    { name: "swordStroked",  type:"image", src: "data/map/swordStroked.png"},
+    { name: "torch",  type:"image", src: "data/map/torch.png"},
+    { name: "trap",  type:"image", src: "data/map/trap.png"},
+    { name: "wallDecor1",  type:"image", src: "data/map/wallDecor1.png"},
+    { name: "wallDecor2",  type:"image", src: "data/map/wallDecor2.png"},
+    { name: "wallDecor3",  type:"image", src: "data/map/wallDecor3.png"},
+    { name: "window1",  type:"image", src: "data/map/window1.png"},
+    { name: "window2",  type:"image", src: "data/map/window2.png"},
+    { name: "window3",  type:"image", src: "data/map/window3.png"},
+    { name: "backgroundArch",  type:"image", src: "data/map/backgroundArch.png"},
+    { name: "backgroundMountain",  type:"image", src: "data/map/backgroundMountain.png"},
+    { name: "backgroundTower",  type:"image", src: "data/map/backgroundTower.png"},
+    { name: "backgroundTree",  type:"image", src: "data/map/backgroundTree.png"},
+    { name: "earthWall2",  type:"image", src: "data/map/earthWall2.png"},
+    { name: "skeleton",  type:"image", src: "data/map/skeleton.png"},
+
+    /* Maps.
+     * @example
+     * { name: "example01", type: "tmx", src: "data/map/example01.tmx" },
+     * { name: "example01", type: "tmx", src: "data/map/example01.json" },
+      */
+    { name: "sandbox", type: "tmx", src: "data/map/sandbox.tmx" },
+    { name: "sandbox2", type: "tmx", src: "data/map/sandbox2.tmx" }
+
+    /* Background music.
+     * @example
+     * { name: "example_bgm", type: "audio", src: "data/bgm/" },
+     */
+
+    /* Sound effects.
+     * @example
+     * { name: "example_sfx", type: "audio", src: "data/sfx/" }
+     */
+
+    /* Atlases
+     * @example
+     * { name: "example_tps", type: "json", src: "data/img/example_tps.json" },
+     */
+
+];
---
+++ examples/sticker-knight/js/screens/play.js
@@ -0,0 +1,9 @@
+game.PlayScreen = me.ScreenObject.extend({
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        // load a level
+        me.levelDirector.loadLevel("sandbox");
+    }
+});
