ca92d95fb02ea9b34a7fd167871998d8dbbe00b1
Olivier Biot <olivier.biot@me.com>
2019-02-04 11:39:45 +0800

    removing this one for now, until it's more complete and cleaned up

--- examples/tower_defense_tutorial/T1-basics/data/maps/art-under-development/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="tiles" tilewidth="101" tileheight="171" tilecount="13" columns="13">
- <image source="training_tiles_image.png" width="1313" height="179"/>
-</tileset>
--- examples/tower_defense_tutorial/T1-basics/data/maps/art-under-development/tiles2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="tiles2" tilewidth="101" tileheight="171" tilecount="13" columns="13">
- <image source="training_tiles_image2.png" width="1313" height="179"/>
-</tileset>
