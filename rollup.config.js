d2c49ce5b9abd369157893d8695f950205d0b8b3
Olivier Biot <olivier.biot@me.com>
2019-03-03 10:01:16 +0800

    [#772] further improvements
    properly include back bable transpiling, and show  bundle size upon completion

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
+import babel from 'rollup-plugin-babel';
 import multiEntry from "rollup-plugin-multi-entry";
 import replace from 'rollup-plugin-replace';
 
 const pkg = require('./package.json');
+const bundleSize = require('rollup-plugin-bundle-size');
 
 export default {
     input: require('./sourceFiles.json'),
@@ -11,7 +13,11 @@ export default {
             '__VERSION__': pkg.version,
             //'/this\._super\(\s*([\w\.]+)\s*,\s*"(\w+)"\s*(,\s*)?/g' : '$1.prototype.$2.apply(this$3',
              delimiters: ['', '']
-        })
+        }),
+        babel({
+          exclude: 'node_modules/**'
+      }),
+      bundleSize()
     ],
     output: {
       file: 'build/melonjs.js',
