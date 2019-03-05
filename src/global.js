e102690485001ef2661499179e9556f3fc1b4dbd
Olivier Biot <olivier.biot@me.com>
2019-03-05 09:23:34 +0800

    clean/fix global definition

--- src/global.js
+++ src/global.js
@@ -14,7 +14,11 @@
 
     // support for AMD (Asynchronous Module Definition) libraries
     if (typeof define === "function" && define.amd) {
-        define(function () { return me; });
+        define([], function() {
+            return {
+                me: me
+            };
+        });
     } // CommonJS and Node.js module support.
     else if (typeof exports !== "undefined") {
         // Support Node.js specific `module.exports` (which can be a function)
@@ -24,7 +28,12 @@
         // CommonJS module 1.1.1 spec (`exports` cannot be a function)
         exports.me = me;
     }
-    // declare me globally
-    global.me = me;
+    // in case AMD not available or unused
+    if (typeof window !== "undefined") {
+        window.me = me;
+    } else if (typeof global !== "undefined") {
+        // Add to global in Node.js (for testing, etc).
+        global.me = me;
+    }
 }(window));
 /* eslint-enable no-undef */
