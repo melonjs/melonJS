8f97eb8fcf44d3d387dc1f10092f8ce964dcc2aa
Olivier Biot <olivier.biot@me.com>
2019-04-02 10:30:36 +0800

    fix the particle editor
    the editor is designed to run with the canvas renderer (as using direcly canvas API)

--- examples/particles/js/game.js
+++ examples/particles/js/game.js
@@ -4,7 +4,7 @@ var game = {
     // Run on page load.
     onload: function () {
         // init the video
-        if (!me.video.init(800, 600, {wrapper : "screen", scale : me.device.devicePixelRatio})) {
+        if (!me.video.init(800, 600, {wrapper : "screen", scale : me.device.devicePixelRatio, renderer : me.video.CANVAS})) {
             alert("Your browser does not support HTML5 canvas.");
             return;
         }
