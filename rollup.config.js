b996c13da8c04fad82ed3a1bb02a9c4b8bdf96d0
Olivier Biot <olivier.biot@me.com>
2020-09-02 17:18:45 +0800

    [#915] generate an es6 module on top of the regular es5 umd one

--- rollup.config.js
+++ rollup.config.js
@@ -18,41 +18,78 @@ const license = [
     ` */`,
 ].join('\n');
 
-export default {
-    input: 'src/index.js',
-    plugins: [
-        resolve({
-            mainFields: ['module', 'main'],
-            browser: true,
-            preferBuiltins: false
-        }),
-        commonjs({
-            include: 'node_modules/**',
-            sourceMap: false
-        }),
-        replace({
-            '__VERSION__': pkg.version,
-            delimiters: ['', '']
+export default [
+    {
+        input: 'src/index.js',
+        plugins: [
+            resolve({
+                mainFields: ['main'],
+                browser: true,
+                preferBuiltins: false
+            }),
+            commonjs({
+                include: 'node_modules/**',
+                sourceMap: false
+            }),
+            replace({
+                '__VERSION__': pkg.version,
+                delimiters: ['', '']
+            }
+            //, {
+            //    '/this\._super\(\s*([\w\.]+)\s*,\s*"(\w+)"\s*(,\s*)?/g' : '$1.prototype.$2.apply(this$3',
+            //    delimiters: ['', '']
+            //  }
+            ),
+            string({
+                include: [
+                    '**/*.frag',
+                    '**/*.vert',
+                ],
+            }),
+            buble(),
+            bundleSize()
+        ],
+        output: {
+          file: 'build/melonjs.js',
+          banner: license,
+          footer : 'me.deprecated.apply();',
+          name : "me",
+          format: 'umd'
         }
-        //, {
-        //    '/this\._super\(\s*([\w\.]+)\s*,\s*"(\w+)"\s*(,\s*)?/g' : '$1.prototype.$2.apply(this$3',
-        //    delimiters: ['', '']
-        //  }
-        ),
-        string({
-            include: [
-                '**/*.frag',
-                '**/*.vert',
-            ],
-        }),
-        buble(),
-        bundleSize()
-    ],
-    output: {
-      file: 'build/melonjs.js',
-      banner: license,
-      footer : 'me.deprecated.apply();',
-      name : "me",
-      format: 'umd'
+    },
+    {
+        input: 'src/index.js',
+        plugins: [
+            resolve({
+                mainFields: ['module'],
+                browser: true,
+                preferBuiltins: false
+            }),
+            commonjs({
+                include: 'node_modules/**',
+                sourceMap: false
+            }),
+            replace({
+                '__VERSION__': pkg.version,
+                delimiters: ['', '']
+            }
+            //, {
+            //    '/this\._super\(\s*([\w\.]+)\s*,\s*"(\w+)"\s*(,\s*)?/g' : '$1.prototype.$2.apply(this$3',
+            //    delimiters: ['', '']
+            //  }
+            ),
+            string({
+                include: [
+                    '**/*.frag',
+                    '**/*.vert',
+                ],
+            }),
+            bundleSize()
+        ],
+        output: {
+          file: 'build/melonjs-module.js',
+          banner: license,
+          format: 'esm'
+        }
     }
-};
+];
