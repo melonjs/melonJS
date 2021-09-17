6167c894dda1300fa4888c8ddc6fb6181173fa91
Olivier Biot <olivier.biot@me.com>
2021-09-17 08:26:27 +0800

    [#987] debug panel documentation update

--- plugins/debug/debugPanel.js
+++ plugins/debug/debugPanel.js
@@ -13,7 +13,11 @@
      * @classdesc
      * a simple debug panel plugin <br>
      * <img src="images/debugPanel.png"/> <br>
-     * the debug panel provides the following information : <br>
+     * <b>usage : </b><br>
+     * &bull; upon loading the debug panel, it will be automatically registered under me.plugins.debugPanel <br>
+     * &bull; you can then press the default "s" key to show or hide the panel, or use me.plugins.debugPanel.show() and me.plugins.debugPanel.show(), or add #debug as a parameter to your URL e.g. http://myURL/index.html#debug <br>
+     * &bull; default key can be configured using the following parameters in the url : e.g. http://myURL/index.html#debugToggleKey=d <br>
+     * <b>the debug panel provides the following information : </b><br>
      * &bull; amount of total objects currently active in the current stage <br>
      * &bull; amount of draws operation <br>
      * &bull; amount of body shape (for collision) <br>
@@ -28,10 +32,6 @@
      * &bull; the hitbox or bounding box for all objects <br>
      * &bull; current velocity vector <br>
      * &bull; quadtree spatial visualization <br>
-     * usage : <br>
-     * &bull; upon loading the debug panel, it will be automatically registered under me.plugins.debugPanel <br>
-     * &bull; you can then press the default "s" key to show or hide the panel, or use me.plugins.debugPanel.show() and me.plugins.debugPanel.show(), or add #debug as a parameter to your URL e.g. http://myURL/index.html#debug <br>
-     * &bull; default key can be configured using the following parameters in the url : e.g. http://myURL/index.html#debugToggleKey=d
      * @class
      * @hideconstructor
      * @name DebugPanel
