79e97c420b85c040c8b0f488cc4e7e35cf3f2a72
Olivier Biot <olivier.biot@me.com>
2022-01-11 10:58:43 +0800

    test the rollup config file syntax through eslint
    modify rollup.config.js accordingly, and also disable the `no-template-curly-in-string` rule

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,39 @@
-import buble from '@rollup/plugin-buble';
-import { string } from 'rollup-plugin-string';
-import replace from '@rollup/plugin-replace';
-import bundleSize from 'rollup-plugin-bundle-size';
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
+import buble from "@rollup/plugin-buble";
+import { string } from "rollup-plugin-string";
+import replace from "@rollup/plugin-replace";
+import bundleSize from "rollup-plugin-bundle-size";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
 
-const pkg = require('./package.json');
+const pkg = require("./package.json");
 
 // credit/license information
 const license = [
-    `/*!`,
-    ` * ${pkg.description} - v${pkg.version}`,
-    ` * http://www.melonjs.org`,
-    ` * ${pkg.name} is licensed under the MIT License.`,
-    ` * http://www.opensource.org/licenses/mit-license`,
-    ` * @copyright (C) 2011 - ${(new Date()).getFullYear()} ${pkg.author.name}`,
-    ` */`,
-].join('\n');
+    "/*!",
+    " * ${pkg.description} - v${pkg.version}",
+    " * http://www.melonjs.org",
+    " * ${pkg.name} is licensed under the MIT License.",
+    " * http://www.opensource.org/licenses/mit-license",
+    " * @copyright (C) 2011 - ${(new Date()).getFullYear()} ${pkg.author.name}",
+    " */"
+].join("\n");
 
 export default [
     {
-        input: 'src/index.js',
+        input: "src/index.js",
         plugins: [
             resolve({
-                mainFields: ['main'],
+                mainFields: ["main"],
                 browser: true,
                 preferBuiltins: false
             }),
             commonjs({
-                include: 'node_modules/**',
+                include: "node_modules/**",
                 sourceMap: false
             }),
             replace({
-                '__VERSION__': pkg.version,
-                delimiters: ['', ''],
+                "__VERSION__": pkg.version,
+                delimiters: ["", ""],
                 preventAssignment : true
             }
             //, {
@@ -43,37 +43,37 @@ export default [
             ),
             string({
                 include: [
-                    '**/*.frag',
-                    '**/*.vert',
-                ],
+                    "**/*.frag",
+                    "**/*.vert"
+                ]
             }),
             buble(),
             bundleSize()
         ],
         output: {
-          file: 'build/melonjs.js',
+          file: "build/melonjs.js",
           banner: license,
-          footer : 'me.deprecated.apply();',
+          footer : "me.deprecated.apply();",
           name : "me",
-          format: 'umd',
-          generatedCode: 'es5'
+          format: "umd",
+          generatedCode: "es5"
         }
     },
     {
-        input: 'src/index.js',
+        input: "src/index.js",
         plugins: [
             resolve({
-                mainFields: ['module'],
+                mainFields: ["module"],
                 browser: true,
                 preferBuiltins: false
             }),
             commonjs({
-                include: 'node_modules/**',
+                include: "node_modules/**",
                 sourceMap: false
             }),
             replace({
-                '__VERSION__': pkg.version,
-                delimiters: ['', ''],
+                "__VERSION__": pkg.version,
+                delimiters: ["", ""],
                 preventAssignment : true
             }
             //, {
@@ -83,16 +83,16 @@ export default [
             ),
             string({
                 include: [
-                    '**/*.frag',
-                    '**/*.vert',
-                ],
+                    "**/*.frag",
+                    "**/*.vert"
+                ]
             }),
             bundleSize()
         ],
         output: {
-          file: 'build/melonjs.module.js',
+          file: "build/melonjs.module.js",
           banner: license,
-          format: 'esm'
+          format: "esm"
         }
     }
 ];
