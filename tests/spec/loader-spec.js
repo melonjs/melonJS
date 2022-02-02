91537185495c3c4326d4cb6ce06cb26e7655a674
Olivier Biot <olivier.biot@me.com>
2022-02-02 15:52:22 +0800

    add a load/unload test case for image assets

--- tests/spec/loader-spec.js
+++ tests/spec/loader-spec.js
@@ -2,22 +2,39 @@ describe("me.loader", function () {
 
     it("should load supported audio assets", function (done) {
 
-        if (me.audio.hasFormat("mp3")|| me.audio.hasFormat("ogg")) {
+        if (me.audio.hasFormat("mp3") || me.audio.hasFormat("ogg")) {
 
             // Initialize audio
             expect(me.audio.init("mp3,ogg")).toEqual(true);
 
-            me.loader.load(
-                {
+            me.loader.load({
                     "name"  : "silence",
                     "type"  : "audio",
                     "src"   : "tests/data/sfx/"
-                },
-                done,
-                function () {
-                    throw new Error("Failed to load `silence`");
-                }
-            );
+            },
+            done,
+            function () {
+                throw new Error("Failed to load `silence`");
+            });
         }
+    });
+
+    it("should load image asset", function () {
+        me.loader.load({
+                "name"  : "testimage",
+                "type"  : "image",
+                "src"   : "tests/data/img/rect.png"
+        },
+        function () {
+            expect(me.loader.getImage("testimage")).not.toEqual(null);
+        },
+        function () {
+            throw new Error("Failed to load `rect.png`");
+        });
+    });
+
+    it("should unload image asset", function () {
+        expect(me.loader.unload({name: "testimage",  type:"image"})).toEqual(true);
+        expect(me.loader.getImage("testimage")).toEqual(null);
     });
 });
