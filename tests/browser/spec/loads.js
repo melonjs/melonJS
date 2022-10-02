222974b83aa0bb503a689f9dcb3321e2b82c1e84
L1lith <lily@l1lith.com>
2022-10-02 13:14:23 -0600

    ensure the library loads

--- tests/browser/spec/loads.js
+++ tests/browser/spec/loads.js
@@ -7,9 +7,9 @@ describe("can load the library", () => {
         const page = await browser.newPage();
         await browser.newPage();
         await page.goto("http://localhost:8042/test.html");
-        const screenshot = await page.screenshot();
-        expect(screenshot).toMatchImageSnapshot(this, {
-            dumpDiffToConsole: true,
-        });
+        const melonLoaded = await page.evaluate(
+            () => typeof window.me == "object" && window.me !== null
+        );
+        expect(melonLoaded).toBe(true);
     });
 });
