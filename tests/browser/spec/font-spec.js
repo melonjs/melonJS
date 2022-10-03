014ac6a550cd648fac5ba7e5702e3b5059ee9e56
L1lith <lily@l1lith.com>
2022-10-02 18:03:42 -0600

    convert the font tests

--- tests/browser/spec/font-spec.js
+++ tests/browser/spec/font-spec.js
@@ -23,41 +23,41 @@ describe("Font : me.Text", function () {
             expect(height).toBe(8);
         });
 
-        // it("default font size is '10'", function () {
-        //     font.setFont("Arial", "10");
-        //     expect(font.height).toEqual(10);
-        // });
+        it("default font size is '10'", async function () {
+            await page.evaluate(() => font.setFont("Arial", "10"));
+            expect(await page.evaluate(() => font.height)).toEqual(10);
+        });
 
-        // it("set font size to 12px", function () {
-        //     font.setFont("Arial", "12px");
-        //     expect(font.height).toEqual(12);
-        // });
+        it("set font size to 12px", async function () {
+            await page.evaluate(() => font.setFont("Arial", "12px"));
+            expect(await page.evaluate(() => font.height)).toEqual(12);
+        });
 
-        // it("set font size to 2ex", function () {
-        //     font.setFont("Arial", "2ex");
-        //     expect(font.height).toEqual(2 * 12);
-        // });
+        it("set font size to 2ex", async function () {
+            await page.evaluate(() => font.setFont("Arial", "2ex"));
+            expect(await page.evaluate(() => font.height)).toEqual(2 * 12);
+        });
 
-        // it("set font size to 1.5em", function () {
-        //     font.setFont("Arial", "1.5em");
-        //     expect(font.height).toEqual(1.5 * 24);
-        // });
+        it("set font size to 1.5em", async function () {
+            await page.evaluate(() => font.setFont("Arial", "1.5em"));
+            expect(await page.evaluate(() => font.height)).toEqual(1.5 * 24);
+        });
 
-        // it("set font size to 18pt", function () {
-        //     font.setFont("Arial", "18pt");
-        //     expect(font.height).toEqual(18 * 0.75);
-        // });
+        it("set font size to 18pt", async function () {
+            await page.evaluate(() => font.setFont("Arial", "18pt"));
+            expect(await page.evaluate(() => font.height)).toEqual(18 * 0.75);
+        });
     });
 
-    //     describe("word wrapping", function () {
-    //         it("word wrap a single string", function () {
-    //             font.wordWrapWidth = 150;
-    //             font.setText(
-    //                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    //             );
-    //             expect(font.measureText().width).toBeLessThanOrEqual(
-    //                 font.wordWrapWidth
-    //             );
-    //         });
+    // describe("word wrapping", function () {
+    //     it("word wrap a single string", function () {
+    //         font.wordWrapWidth = 150;
+    //         font.setText(
+    //             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    //         );
+    //         expect(font.measureText().width).toBeLessThanOrEqual(
+    //             font.wordWrapWidth
+    //         );
     //     });
+    // });
 });
