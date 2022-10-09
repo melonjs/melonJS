8ec0b2f0608f13dfca916c4f66c967b6b5791b63
L1lith <lily@l1lith.com>
2022-10-08 18:53:14 -0600

    fix the build

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@ import image from "@rollup/plugin-image";
 import bundleSize from "rollup-plugin-bundle-size";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import multiInput from "rollup-plugin-multi-input";
 
 const pkg = require("./package.json");
-
 
 // credit/license information
 const license = [
@@ -15,42 +15,40 @@ const license = [
     " * http://www.melonjs.org",
     " * " + pkg.name + " is licensed under the MIT License.",
     " * http://www.opensource.org/licenses/mit-license",
-    " * @copyright (C) 2011 - " + (new Date()).getFullYear() + " " + pkg.author,
-    " */"
+    " * @copyright (C) 2011 - " + new Date().getFullYear() + " " + pkg.author,
+    " */",
 ].join("\n");
 
 export default [
     {
-        input: "src/index.js",
+        input: "src/**/*.js",
         plugins: [
-            resolve({
-                mainFields: ["module"],
-                browser: true,
-                preferBuiltins: false
-            }),
-            commonjs({
-                include: "node_modules/**",
-                sourceMap: false
-            }),
+            multiInput(),
+            // resolve({
+            //     mainFields: ["module"],
+            //     browser: true,
+            //     preferBuiltins: false,
+            // }),
+            // commonjs({
+            //     include: "node_modules/**",
+            //     sourceMap: false,
+            // }),
             replace({
                 values: {
-                    __VERSION__: pkg.version
+                    __VERSION__: pkg.version,
                 },
-                preventAssignment: true
+                preventAssignment: true,
             }),
             string({
-                include: [
-                    "**/*.frag",
-                    "**/*.vert"
-                ]
+                include: ["**/*.frag", "**/*.vert"],
             }),
             image(),
-            bundleSize()
+            //bundleSize(),
         ],
         output: {
-          file: "build/melonjs.module.js",
-          banner: license,
-          format: "esm"
-        }
-    }
+            dir: "build/",
+            banner: license,
+            format: "esm",
+        },
+    },
 ];
