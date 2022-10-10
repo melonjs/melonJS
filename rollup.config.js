d60d2ff2aa54d7a5cce6f0b3258425c20fac9879
Olivier Biot <olivier.biot@me.com>
2022-10-10 14:26:22 +0800

    [#1142] generate a new `melonjs.mjs` tree-shakeable build

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import image from "@rollup/plugin-image";
 import bundleSize from "rollup-plugin-bundle-size";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
+import rename from "rollup-plugin-rename";
 
 const pkg = require("./package.json");
 
@@ -20,6 +21,7 @@ const license = [
 ].join("\n");
 
 export default [
+    // ES6 "melonjs.module.js" bundle
     {
         input: "src/index.js",
         plugins: [
@@ -51,6 +53,48 @@ export default [
           file: "build/melonjs.module.js",
           banner: license,
           format: "esm"
+        }
+    },
+    // ES6 "melonjs.mjs/" chunk
+    {
+        input: "src/index.js",
+        plugins: [
+            resolve({
+                mainFields: ["module"],
+                browser: true,
+                preferBuiltins: false
+            }),
+            commonjs({
+                include: "node_modules/**",
+                sourceMap: false
+            }),
+            replace({
+                values: {
+                    __VERSION__: pkg.version
+                },
+                preventAssignment: true
+            }),
+            string({
+                include: [
+                    "**/*.frag",
+                    "**/*.vert"
+                ]
+            }),
+            image(),
+            // workaround while waiting for rollup 3 to be released
+            // @see https://github.com/rollup/rollup/issues/3684
+            rename({
+                include: ["**/*.js"],
+                map: (name) => name
+                    .replace("src/", "")
+                    .replace("node_modules/", "external/")
+            })
+        ],
+        output: {
+          dir: "build/melonjs.mjs",
+          banner: license,
+          format: "es",
+          preserveModules: true
         }
     }
 ];
