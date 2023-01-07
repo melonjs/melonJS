1af2b575a7f3c6f986b5b6ca9585bedb92207325
Olivier Biot <olivier.biot@me.com>
2023-01-07 16:30:43 +0800

    [#1151] fix a regression with the `collision.rayCast()` function (thanks @cpravetz)

--- src/physics/collision.js
+++ src/physics/collision.js
@@ -117,7 +117,7 @@ var collision = {
      *        // ...
      *    }
      */
-    rayCast(line, result) { return game.world.rayCast(line, result); }
+    rayCast(line, result) { return game.world.detector.rayCast(line, result); }
 };
 
 export default collision;
