68b94bcdfb6d617e03c4f56e42555067bd5e9bb5
Olivier Biot <olivier.biot@me.com>
2023-12-18 12:59:13 +0700

    remove "dead" duplicated code

--- src/loader/parsers/binary.js
+++ src/loader/parsers/binary.js
@@ -1,5 +1,4 @@
 import { binList } from "../cache.js";
-import { crossOrigin } from "../settings.js";
 import { fetchData } from "./fetchdata.js";
 
 /**
@@ -28,42 +27,6 @@ export function preloadBinary(data, onload, onerror) {
                 onerror(error);
             }
         });
-
-    return 1;
-}
-
-/**
- * parse/preload a Javascript files
- * @param {loader.Asset} data - asset data
- * @param {Function} [onload] - function to be called when the asset is loaded
- * @param {Function} [onerror] - function to be called in case of error
- * @ignore
- */
-export function preloadJavascript(data, onload, onerror) {
-    let script = document.createElement("script");
-
-    script.src = data.src;
-    script.type = "text/javascript";
-    if (typeof (crossOrigin) === "string") {
-        script.crossOrigin = crossOrigin;
-    }
-    script.defer = true;
-
-    if (typeof onload === "function") {
-        script.onload = () => {
-            // callback
-            onload();
-        };
-    }
-
-    if (typeof onerror === "function") {
-        script.onerror = () => {
-            // callback
-            onerror(data.name);
-        };
-    }
-
-    document.getElementsByTagName("body")[0].appendChild(script);
 
     return 1;
 }
