9bb1899409471749b97527bc9a78d9211810e5d1
Olivier Biot <olivier.biot@me.com>
2024-02-05 18:55:50 +0800

    add a video sprite example in the documentation

--- src/renderable/sprite.js
+++ src/renderable/sprite.js
@@ -43,6 +43,16 @@ export default class Sprite extends Renderable {
      *     image : mytexture,
      *     region : "npc2.png",
      * });
+     *
+     * // create a video sprite
+     * let videoSprite = new me.Sprite(0, 0, {
+     *     image : me.loader.getVideo("bigbunny"),
+     *     anchorPoint : new me.Vector2d(0.5, 0.5)
+     * });
+     * // scale the video sprite
+     * videoSprite.currentTransform.scale(2);
+     * // start playing the video (if video is preloaded with `autoplay` set to false)
+     * videoSprite.play();
      */
     constructor(x, y, settings) {
 
