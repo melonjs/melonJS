f1fff09bd490255672698a39908b1e0feba62d4d
waltibaba <walter@sereinig.com>
2024-02-13 11:11:57 +0100

    better doc generation with error catching
    npm run doc is still broken, inexplicably
    running the npm run doc command manually works perfectly

--- docker/generate-docs.sh
+++ docker/generate-docs.sh
@@ -1,4 +1,7 @@
-docker build .. -f Dockerfile -t melonjs:latest --no-cache 
-docker run --name melonjs-doc melonjs:latest npm run doc-prod
-docker cp melonjs-doc:/docs/ ../
-docker rm melonjs-doc
+#!/bin/bash
+docker build . -f docker/Dockerfile -t melonjs:latest --no-cache && \
+docker run -it --rm --name melonjs-doc -d melonjs:latest  /bin/bash && \
+docker exec melonjs-doc npm install && \
+docker exec melonjs-doc npm run doc-prod && \
+docker cp melonjs-doc:/docs/ .
+docker stop melonjs-doc
