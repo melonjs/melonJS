fc5bfd0cf7f63102bc83d480425a41707064ff08
waltibaba <walter@sereinig.com>
2024-02-13 10:09:14 +0100

    doc gen: docker dependencies, newer base container
    added devdependency on docker for npm run doc
    changed dockerfile to use existing local repo instead of cloning master from remote
    deriving dockerfile from nodejs instead of manual install

--- docker/generate-docs.sh
+++ docker/generate-docs.sh
@@ -1,4 +1,4 @@
-docker build . -t melonjs:latest --no-cache
+docker build .. -f Dockerfile -t melonjs:latest --no-cache 
 docker run --name melonjs-doc melonjs:latest npm run doc-prod
 docker cp melonjs-doc:/docs/ ../
 docker rm melonjs-doc
