6fbbeffacc21df7fab325192e0e706322e7f8712
Olivier Biot <olivier.biot@me.com>
2024-06-21 12:56:06 +0800

    more TypeScript declaration fixes

--- src/geometries/poly.js
+++ src/geometries/poly.js
@@ -3,6 +3,9 @@ import pool from "./../system/pooling.js";
 
 /**
  * @import Vector2d from "./../math/vector2.js";
+ * @import ObservableVector2d from "./../math/observable_vector2.js";
+ * @import Matrix2d from "./../math/matrix2.js";
+ * @import Bounds from "./../physics/bounds.js";
  */
 
 /**
