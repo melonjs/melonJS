2f250093a28333e9cb6b44c2270c6638a0f7a74a
Olivier Biot <olivier.biot@me.com>
2024-07-16 16:17:15 +0800

    remove console.log

--- packages/melonjs/src/level/level.js
+++ packages/melonjs/src/level/level.js
@@ -63,7 +63,6 @@ function safeLoadLevel(levelId, options, restart) {
  */
 function loadTMXLevel(levelId, container, flatten, setViewportBounds) {
 	const level = levels[levelId];
-	console.log(level);
 	// reset the GUID generator
 	// and pass the level id as parameter
 	resetGUID(levelId, level.nextobjectid);
