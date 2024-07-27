d9b69466f2345fa54c9ee8bf0e03c536a9934f39
Olivier Biot <olivier.biot@me.com>
2024-07-27 11:57:53 +0800

    optimize pool implementation (for performance)
    remove previous inUse array, and replaced by a counter to keep track of "used" instances

--- packages/melonjs/src/system/pool.ts
+++ packages/melonjs/src/system/pool.ts
@@ -3,6 +3,7 @@ export interface Pool<T, A extends unknown[]> {
 	release(object: T): void;
 	purge(): void;
 	size(): number;
+	used(): number;
 }
 
 type Reset<A extends unknown[]> = ((...args: A) => void) | undefined;
@@ -16,38 +17,54 @@ export const createPool = <T, A extends unknown[]>(
 	options: (...args: A) => CreatePoolOptions<T, A>,
 ): Pool<T, A> => {
 	const available: T[] = [];
-	const inUse: T[] = [];
 	const instanceResetMethods = new Map<T, Reset<A>>();
+	let inUse: number = 0;
 
 	return {
-		release: (object: T) => {
-			const index = inUse.indexOf(object);
-			if (index >= 0) {
-				const [instance] = inUse.splice(index, 1);
-				available.push(instance);
-			} else {
-				throw new Error("Trying to release an instance that is not in use.");
-			}
+		/**
+		 * release an object back to the pool
+		 * @param instance The object to release.
+		 */
+		release: (instance: T) => {
+			available.push(instance);
+			inUse--;
 		},
+		/**
+		 * get an instance from the pool
+		 * @param args The arguments for creating the instance.
+		 */
 		get: (...args) => {
 			const object = available.pop();
 			if (object) {
-				inUse.push(object);
 				const reset = instanceResetMethods.get(object);
 				reset?.(...args);
+				inUse++;
 				return object;
 			} else {
 				const { instance, reset } = options(...args);
 				instanceResetMethods.set(instance, reset);
-				inUse.push(instance);
+				inUse++;
 				return instance;
 			}
 		},
+		/**
+		 * purge the pool
+		 */
 		purge: () => {
 			available.length = 0;
+			inUse = 0;
 		},
+		/**
+		 * get the current size of the pool (how many objects are available)
+		 */
 		size: () => {
-			return available.length + inUse.length;
+			return available.length;
+		},
+		/**
+		 * get the number of objects currently in use
+		 */
+		used: () => {
+			return inUse;
 		},
 	};
 };
