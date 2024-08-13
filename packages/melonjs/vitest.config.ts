61367214e7d9e970039b9c787cee2fe3f637db24
Olivier Biot <olivier.biot@me.com>
2024-08-14 07:52:46 +0800

    fix linting

--- packages/melonjs/vitest.config.ts
+++ packages/melonjs/vitest.config.ts
@@ -1,5 +1,7 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from "vitest/config";
-import glsl from "vite-plugin-glsl";
+import { default as glsl } from "vite-plugin-glsl";
 import { PackageJson } from "type-fest";
 
 const packageJson = (
@@ -13,28 +15,30 @@ if (!version) {
 	throw new Error("Version missing from package.json");
 }
 
-export default defineConfig({
-	test: {
-		include: ["**/*.{test,spec}.[jt]s?(x)"],
-		browser: {
-			enabled: true,
-			name: "chromium",
-			provider: "playwright",
-			headless: true,
-			screenshotFailures: false,
-		},
-	},
-	publicDir: "./tests/public",
-	plugins: [
-		glsl(),
-		{
-			name: "transform-file",
-			transform(src) {
-				return {
-					code: src.replace(/=\s__VERSION__/g, `= "${version}"`),
-					map: null,
-				};
+export default defineConfig(() =>
+	defineConfig({
+		test: {
+			include: ["**/*.{test,spec}.[jt]s?(x)"],
+			browser: {
+				enabled: true,
+				name: "chromium",
+				provider: "playwright",
+				headless: true,
+				screenshotFailures: false,
 			},
 		},
-	],
-});
+		publicDir: "./tests/public",
+		plugins: [
+			glsl() as Plugin,
+			{
+				name: "transform-file",
+				transform(src) {
+					return {
+						code: src.replace(/=\s__VERSION__/g, `= "${version}"`),
+						map: null,
+					};
+				},
+			},
+		],
+	}),
+);
